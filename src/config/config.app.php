d909eb357873500e794e1794971970bf60f4ba34
George Lewe <george@lewe.com>
2023-06-29 16:05:52 +0200

    v3.7.4 info

--- src/config/config.app.php
+++ src/config/config.app.php
@@ -158,8 +158,8 @@ define('LDAP_TLS', 0); // To avoid "Undefined index: LDAP_TLS" error message for
  * !Do not change anything below this line. It is protected by the license agreement!
  */
 define('APP_NAME', "TeamCal Neo");
-define('APP_VER', "3.7.3");
-define('APP_DATE', "2023-06-2711");
+define('APP_VER', "3.7.4");
+define('APP_DATE', "2023-06-29");
 define('APP_YEAR', "2014-" . date('Y'));
 define('APP_AUTHOR', "George Lewe");
 define('APP_URL', "http://www.lewe.com");
