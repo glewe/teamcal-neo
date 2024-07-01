456874d99ce1fb3e9a8250887d84e00bea61b06b
George Lewe <george@lewe.com>
2024-07-01 15:02:43 +0200

    Code maintenance

--- src/classes/Controller.class.php
+++ src/classes/Controller.class.php
@@ -1,11 +1,12 @@
 <?php
 
 if (!defined('VALID_ROOT')) exit('');
+
 /**
  * Controller
  *
  * This class provides methods and properties for application controllers.
- * 
+ *
  * @author George Lewe <george@lewe.com>
  * @copyright Copyright (c) 2014-2023 by George Lewe
  * @link https://www.lewe.com
@@ -14,46 +15,44 @@ if (!defined('VALID_ROOT')) exit('');
  * @subpackage Calendar Management
  * @since 3.0.0
  */
-class Controller
-{
-    public $name = '';
-    public $faIcon = 'folder-o';
-    public $iconColor = 'default';
-    public $panelColor = 'default';
-    public $permission = '';
-    public $title = '';
-    public $docurl = '';
+class Controller {
+  public $name = '';
+  public $faIcon = 'folder-o';
+  public $iconColor = 'default';
+  public $panelColor = 'default';
+  public $permission = '';
+  public $title = '';
+  public $docurl = '';
 
-    // ---------------------------------------------------------------------
-    /**
-     * Constructor
-     * 
-     * @param string $name Controller name
-     * @param string $faIcon Fontawesome icon (w/o prefix "fa_")
-     * @param string $iconColor Bootstrap color name for icon text color
-     * @param string $panelColor Bootstrap color name for panel header background color
-     * @param string $permission The permission a user role must have to access this controller. Leave empty for public access.
-     * @param string $docurl URL to the documentation of this controller
-     * 
-     */
-    public function __construct($name, $faIcon, $iconColor, $panelColor, $permission, $title, $docurl)
-    {
-        if (!strlen($name)) {
-            /**
-             * No name passed
-             */
-            $errorData['title'] = 'Application Error';
-            $errorData['subject'] = 'Controller Instance';
-            $errorData['text'] = 'The controller instance could not be initiated due to a missing controller name.';
-            require(WEBSITE_ROOT . '/views/error.php');
-            die();
-        }
-        $this->name = $name;
-        $this->faIcon = $faIcon;
-        $this->iconColor = $iconColor;
-        $this->panelColor = $panelColor;
-        $this->permission = $permission;
-        $this->title = $title;
-        $this->docurl = $docurl;
+  // ---------------------------------------------------------------------
+  /**
+   * Constructor
+   *
+   * @param string $name Controller name
+   * @param string $faIcon Fontawesome icon (w/o prefix "fa_")
+   * @param string $iconColor Bootstrap color name for icon text color
+   * @param string $panelColor Bootstrap color name for panel header background color
+   * @param string $permission The permission a user role must have to access this controller. Leave empty for public access.
+   * @param string $docurl URL to the documentation of this controller
+   *
+   */
+  public function __construct($name, $faIcon, $iconColor, $panelColor, $permission, $title, $docurl) {
+    if (!strlen($name)) {
+      /**
+       * No name passed
+       */
+      $errorData['title'] = 'Application Error';
+      $errorData['subject'] = 'Controller Instance';
+      $errorData['text'] = 'The controller instance could not be initiated due to a missing controller name.';
+      require(WEBSITE_ROOT . "/views/error.php");
+      die();
     }
+    $this->name = $name;
+    $this->faIcon = $faIcon;
+    $this->iconColor = $iconColor;
+    $this->panelColor = $panelColor;
+    $this->permission = $permission;
+    $this->title = $title;
+    $this->docurl = $docurl;
+  }
 }
--- src/controller/attachments.php
+++ src/controller/attachments.php
@@ -20,13 +20,13 @@ if (!defined('VALID_ROOT')) exit('No direct access allowed!');
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission)) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require(WEBSITE_ROOT . '/controller/alert.php');
+  die();
 }
 
 //=============================================================================
@@ -35,7 +35,7 @@ if (!isAllowed($CONF['controllers'][$controller]->permission)) {
 //
 $UPL = new Upload();
 $AT = new Attachment();
-$UAT =  new UserAttachment();
+$UAT = new UserAttachment();
 
 //=============================================================================
 //
@@ -55,181 +55,184 @@ $viewData['uplFiles'] = array();
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    //
-    // Sanitize input
-    //
-    $_POST = sanitize($_POST);
+  //
+  // Sanitize input
+  //
+  $_POST = sanitize($_POST);
 
-    //
-    // Form validation
-    //
-    $inputError = false;
+  //
+  // Form validation
+  //
+  $inputError = false;
 
-    //
-    // Validate input data. If something is wrong or missing, set $inputError = true
-    //
+  //
+  // Validate input data. If something is wrong or missing, set $inputError = true
+  //
 
-    if (!$inputError) {
-        // ,-------------,
-        // | Upload File |
-        // '-------------'
-        if (isset($_POST['btn_uploadFile'])) {
-            $UPL->upload_dir = $uplDir;
-            $UPL->extensions = $CONF['uplExtensions'];
-            $UPL->do_filename_check = "y";
-            $UPL->replace = "y";
-            $UPL->the_temp_file = $_FILES['file_image']['tmp_name'];
-            $UPL->the_file = $_FILES['file_image']['name'];
-            $UPL->http_error = $_FILES['file_image']['error'];
+  if (!$inputError) {
+    // ,-------------,
+    // | Upload File |
+    // '-------------'
+    if (isset($_POST['btn_uploadFile'])) {
+      $UPL->upload_dir = $uplDir;
+      $UPL->extensions = $CONF['uplExtensions'];
+      $UPL->do_filename_check = "y";
+      $UPL->replace = "y";
+      $UPL->the_temp_file = $_FILES['file_image']['tmp_name'];
+      $UPL->the_file = $_FILES['file_image']['name'];
+      $UPL->http_error = $_FILES['file_image']['error'];
 
-            if ($UPL->upload()) {
-                $AT->create($UPL->the_file, $UL->username);
-                $fileid = $AT->getId($UPL->the_file);
-                $UAT->create($UL->username, $fileid);
+      if ($UPL->upload()) {
+        $AT->create($UPL->the_file, $UL->username);
+        $fileid = $AT->getId($UPL->the_file);
+        $UAT->create($UL->username, $fileid);
 
-                switch ($_POST['opt_shareWith']) {
-                    case "admin":
-                        $UAT->create('admin', $fileid);
-                        break;
+        switch ($_POST['opt_shareWith']) {
+          case "admin":
+            $UAT->create('admin', $fileid);
+            break;
 
-                    case "all":
-                        $users = $U->getAll();
-                        foreach ($users as $user) $UAT->create($user['username'], $fileid);
-                        break;
+          case "all":
+            $users = $U->getAll();
+            foreach ($users as $user) $UAT->create($user['username'], $fileid);
+            break;
 
-                    case "group":
-                        if (isset($_POST['sel_shareWithGroup'])) {
-                            foreach ($_POST['sel_shareWithGroup'] as $gto) {
-                                $groupusers = $UG->getAllForGroup($gto);
-                                foreach ($groupusers as $groupuser) {
-                                    $UAT->create($groupuser['username'], $fileid);
-                                }
-                            }
-                        } else {
-                            //
-                            // No group selected
-                            //
-                            $showAlert = TRUE;
-                            $alertData['type'] = 'warning';
-                            $alertData['title'] = $LANG['alert_warning_title'];
-                            $alertData['subject'] = $LANG['msg_no_group_subject'];
-                            $alertData['text'] = $LANG['msg_no_group_text'];
-                            $alertData['help'] = '';
-                        }
-                        break;
-
-                    case "role":
-                        if (isset($_POST['sel_shareWithRole'])) {
-                            foreach ($_POST['sel_shareWithRole'] as $rto) {
-                                $roleusers = $U->getAllForRole($rto);
-                                foreach ($roleusers as $roleuser) {
-                                    $UAT->create($roleuser['username'], $fileid);
-                                }
-                            }
-                        } else {
-                            //
-                            // No group selected
-                            //
-                            $showAlert = TRUE;
-                            $alertData['type'] = 'warning';
-                            $alertData['title'] = $LANG['alert_warning_title'];
-                            $alertData['subject'] = $LANG['msg_no_group_subject'];
-                            $alertData['text'] = $LANG['msg_no_group_text'];
-                            $alertData['help'] = '';
-                        }
-                        break;
-
-                    case "user":
-                        if (isset($_POST['sel_shareWithUser'])) {
-                            foreach ($_POST['sel_shareWithUser'] as $uto) $UAT->create($uto, $fileid);
-                        } else {
-                            //
-                            // No user selected
-                            //
-                            $showAlert = TRUE;
-                            $alertData['type'] = 'warning';
-                            $alertData['title'] = $LANG['alert_warning_title'];
-                            $alertData['subject'] = $LANG['msg_no_user_subject'];
-                            $alertData['text'] = $LANG['msg_no_user_text'];
-                            $alertData['help'] = '';
-                        }
-                        break;
+          case "group":
+            if (isset($_POST['sel_shareWithGroup'])) {
+              foreach ($_POST['sel_shareWithGroup'] as $gto) {
+                $groupusers = $UG->getAllForGroup($gto);
+                foreach ($groupusers as $groupuser) {
+                  $UAT->create($groupuser['username'], $fileid);
                 }
-
-                if (!$showAlert) {
-                    //
-                    // Log this event
-                    //
-                    $LOG->log("logUpload", $UL->username, "log_upload_image", $UPL->uploaded_file['name']);
-                    header("Location: index.php?action=" . $controller);
-                }
+              }
             } else {
-                //
-                // Upload failed
-                //
-                $showAlert = true;
-                $alertData['type'] = 'danger';
-                $alertData['title'] = $LANG['alert_danger_title'];
-                $alertData['subject'] = $LANG['alert_upl_img_subject'];
-                $alertData['text'] = $UPL->getErrors();
-                $alertData['help'] = '';
+              //
+              // No group selected
+              //
+              $showAlert = TRUE;
+              $alertData['type'] = 'warning';
+              $alertData['title'] = $LANG['alert_warning_title'];
+              $alertData['subject'] = $LANG['msg_no_group_subject'];
+              $alertData['text'] = $LANG['msg_no_group_text'];
+              $alertData['help'] = '';
             }
-        }
-        // ,-------------,
-        // | Delete File |
-        // '-------------'
-        elseif (isset($_POST['btn_deleteFile'])) {
-            if (isset($_POST['chk_file'])) {
-                $selected_files = $_POST['chk_file'];
-                foreach ($selected_files as $file) {
-                    if ($UL->username == 'admin' or $UL->username == $AT->getUploader($file)) {
-                        $fileid = $AT->getId($file);
-                        $AT->delete($file);
-                        $UAT->deleteFile($fileid);
-                        unlink($uplDir . $file);
-                    }
+            break;
+
+          case "role":
+            if (isset($_POST['sel_shareWithRole'])) {
+              foreach ($_POST['sel_shareWithRole'] as $rto) {
+                $roleusers = $U->getAllForRole($rto);
+                foreach ($roleusers as $roleuser) {
+                  $UAT->create($roleuser['username'], $fileid);
                 }
+              }
             } else {
-                //
-                // No file selected
-                //
-                $showAlert = true;
-                $alertData['type'] = 'warning';
-                $alertData['title'] = $LANG['alert_warning_title'];
-                $alertData['subject'] = $LANG['msg_no_file_subject'];
-                $alertData['text'] = $LANG['msg_no_file_text'];
-                $alertData['help'] = '';
+              //
+              // No group selected
+              //
+              $showAlert = TRUE;
+              $alertData['type'] = 'warning';
+              $alertData['title'] = $LANG['alert_warning_title'];
+              $alertData['subject'] = $LANG['msg_no_group_subject'];
+              $alertData['text'] = $LANG['msg_no_group_text'];
+              $alertData['help'] = '';
             }
-        }
-        // ,---------------,
-        // | Update Shares |
-        // '---------------'
-        $files = $AT->getAll();
-        foreach ($files as $file) {
-            if (isset($_POST['btn_updateShares' . $file['id']])) {
-                $UAT->deleteFile($file['id']);
-                if (isset($_POST['sel_shares' . $file['id']])) {
-                    foreach ($_POST['sel_shares' . $file['id']] as $uto) $UAT->create($uto, $file['id']);
-                }
-                // Make sure the uploader has access
-                $UAT->create($AT->getUploaderById($file['id']), $file['id']);
-            } else if (isset($_POST['btn_clearShares' . $file['id']])) {
-                $UAT->deleteFile($file['id']);
-                // Make sure the uploader has access
-                $UAT->create($AT->getUploaderById($file['id']), $file['id']);
+            break;
+
+          case "user":
+            if (isset($_POST['sel_shareWithUser'])) {
+              foreach ($_POST['sel_shareWithUser'] as $uto) $UAT->create($uto, $fileid);
+            } else {
+              //
+              // No user selected
+              //
+              $showAlert = TRUE;
+              $alertData['type'] = 'warning';
+              $alertData['title'] = $LANG['alert_warning_title'];
+              $alertData['subject'] = $LANG['msg_no_user_subject'];
+              $alertData['text'] = $LANG['msg_no_user_text'];
+              $alertData['help'] = '';
             }
+            break;
+
+          default:
+            break;
         }
-    } else {
+
+        if (!$showAlert) {
+          //
+          // Log this event
+          //
+          $LOG->log("logUpload", $UL->username, "log_upload_image", $UPL->uploaded_file['name']);
+          header("Location: index.php?action=" . $controller);
+        }
+      } else {
         //
-        // Input validation failed
+        // Upload failed
         //
-        $showAlert = TRUE;
+        $showAlert = true;
         $alertData['type'] = 'danger';
         $alertData['title'] = $LANG['alert_danger_title'];
-        $alertData['subject'] = $LANG['alert_input'];
-        $alertData['text'] = $LANG['register_alert_failed'];
+        $alertData['subject'] = $LANG['alert_upl_img_subject'];
+        $alertData['text'] = $UPL->getErrors();
         $alertData['help'] = '';
+      }
     }
+    // ,-------------,
+    // | Delete File |
+    // '-------------'
+    elseif (isset($_POST['btn_deleteFile'])) {
+      if (isset($_POST['chk_file'])) {
+        $selected_files = $_POST['chk_file'];
+        foreach ($selected_files as $file) {
+          if ($UL->username == 'admin' or $UL->username == $AT->getUploader($file)) {
+            $fileid = $AT->getId($file);
+            $AT->delete($file);
+            $UAT->deleteFile($fileid);
+            unlink($uplDir . $file);
+          }
+        }
+      } else {
+        //
+        // No file selected
+        //
+        $showAlert = true;
+        $alertData['type'] = 'warning';
+        $alertData['title'] = $LANG['alert_warning_title'];
+        $alertData['subject'] = $LANG['msg_no_file_subject'];
+        $alertData['text'] = $LANG['msg_no_file_text'];
+        $alertData['help'] = '';
+      }
+    }
+    // ,---------------,
+    // | Update Shares |
+    // '---------------'
+    $files = $AT->getAll();
+    foreach ($files as $file) {
+      if (isset($_POST['btn_updateShares' . $file['id']])) {
+        $UAT->deleteFile($file['id']);
+        if (isset($_POST['sel_shares' . $file['id']])) {
+          foreach ($_POST['sel_shares' . $file['id']] as $uto) $UAT->create($uto, $file['id']);
+        }
+        // Make sure the uploader has access
+        $UAT->create($AT->getUploaderById($file['id']), $file['id']);
+      } else if (isset($_POST['btn_clearShares' . $file['id']])) {
+        $UAT->deleteFile($file['id']);
+        // Make sure the uploader has access
+        $UAT->create($AT->getUploaderById($file['id']), $file['id']);
+      }
+    }
+  } else {
+    //
+    // Input validation failed
+    //
+    $showAlert = TRUE;
+    $alertData['type'] = 'danger';
+    $alertData['title'] = $LANG['alert_danger_title'];
+    $alertData['subject'] = $LANG['alert_input'];
+    $alertData['text'] = $LANG['register_alert_failed'];
+    $alertData['help'] = '';
+  }
 }
 
 //=============================================================================
@@ -240,12 +243,12 @@ $viewData['upl_maxsize'] = $CONF['uplMaxsize'];
 $viewData['upl_formats'] = implode(', ', $CONF['uplExtensions']);
 $files = getFiles(APP_UPL_DIR, $CONF['uplExtensions'], null);
 foreach ($files as $file) {
-    $fid = $AT->getId($file);
-    if ($UL->username != 'admin') {
-        if ($UAT->hasAccess($UL->username, $fid)) $viewData['uplFiles'][] = array('fid' => $fid, 'fname' => $file);
-    } else {
-        $viewData['uplFiles'][] = array('fid' => $fid, 'fname' => $file);
-    }
+  $fid = $AT->getId($file);
+  if ($UL->username != 'admin') {
+    if ($UAT->hasAccess($UL->username, $fid)) $viewData['uplFiles'][] = array( 'fid' => $fid, 'fname' => $file );
+  } else {
+    $viewData['uplFiles'][] = array( 'fid' => $fid, 'fname' => $file );
+  }
 }
 $viewData['groups'] = $G->getAll();
 $viewData['roles'] = $RO->getAll();
