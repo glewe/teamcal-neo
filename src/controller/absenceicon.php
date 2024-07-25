0f33c7355347c439c219cb63bdaa329a31f3f256
George Lewe <george@lewe.com>
2024-07-25 15:36:41 +0200

    Sonar fixes (absenceicon controller)

--- src/controller/absenceicon.php
+++ src/controller/absenceicon.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Absence Icon Controller
  *
@@ -12,20 +14,18 @@ if (!defined('VALID_ROOT')) { exit(''); }
  * @since 3.0.0
  */
 
-// echo '<script type="text/javascript">alert("Debug: ");</script>';
-
 //=============================================================================
 //
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission)) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -35,25 +35,23 @@ if (!isAllowed($CONF['controllers'][$controller]->permission)) {
 $AA = new Absences(); // for the absence type to be edited
 
 if (isset($_GET['id'])) {
-    $missingData = FALSE;
-    $id = sanitize($_GET['id']);
-    if (!$AA->get($id)) $missingData = TRUE;
+  $missingData = false;
+  $id = sanitize($_GET['id']);
+  if (!$AA->get($id)) {
+    $missingData = true;
+  }
 } else {
-    $missingData = TRUE;
+  $missingData = true;
 }
 
 if ($missingData) {
-    /**
-     * URL param fail
-     */
-    $alertData['type'] = 'danger';
-    $alertData['title'] = $LANG['alert_danger_title'];
-    $alertData['subject'] = $LANG['alert_no_data_subject'];
-    $alertData['text'] = $LANG['alert_no_data_text'];
-    $alertData['help'] = $LANG['alert_no_data_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
-} else {
+  $alertData['type'] = 'danger';
+  $alertData['title'] = $LANG['alert_danger_title'];
+  $alertData['subject'] = $LANG['alert_no_data_subject'];
+  $alertData['text'] = $LANG['alert_no_data_text'];
+  $alertData['help'] = $LANG['alert_no_data_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -72,34 +70,36 @@ $inputAlert = array();
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    // ,------,
-    // | Save |
-    // '------'
-    if (isset($_POST['btn_save'])) {
-        $AA->id = $_POST['hidden_id'];
-        if (isset($_POST['opt_absIcon'])) $AA->icon = $_POST['opt_absIcon'];
-        else $AA->icon = 'times';
-
-        //
-        // Update the record
-        //
-        $AA->update($AA->id);
-
-        //
-        // Go back to absence edit page
-        //
-        header("Location: index.php?action=" . $controller . "&id=" . $AA->id);
+  // ,------,
+  // | Save |
+  // '------'
+  if (isset($_POST['btn_save'])) {
+    $AA->id = $_POST['hidden_id'];
+    if (isset($_POST['opt_absIcon'])) {
+      $AA->icon = $_POST['opt_absIcon'];
+    } else {
+      $AA->icon = 'times';
     }
+
+    //
+    // Update the record
+    //
+    $AA->update($AA->id);
+
+    //
+    // Go back to absence edit page
+    //
+    header("Location: index.php?action=" . $controller . "&id=" . $AA->id);
+  } elseif (isset($_POST['btn_fa_filter'])) {
     // ,--------,
     // | Filter |
     // '--------'
-    else if (isset($_POST['btn_fa_filter'])) {
-      $filterString = $_POST['fa_search'];
-      $allIcons = $faIcons;
-      $faIcons = array_filter($allIcons, function ($element) use ($filterString) {
-        return strpos($element, $filterString) !== false;
-      });
-    }
+    $filterString = $_POST['fa_search'];
+    $allIcons = $faIcons;
+    $faIcons = array_filter($allIcons, function ($element) use ($filterString) {
+      return strpos($element, $filterString) !== false;
+    });
+  }
 }
 
 //=============================================================================
@@ -110,19 +110,23 @@ $viewData['id'] = $AA->id;
 $viewData['name'] = $AA->name;
 $viewData['icon'] = $AA->icon;
 
-//dnd($faIcons);
-
 foreach ($faIcons as $faIcon) {
-    if (strstr($faIcon, "fa-brands ")) $viewData['fabIcons'][] = array('val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false);
-    if (strstr($faIcon, "fa-regular ")) $viewData['farIcons'][] = array('val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false);
-    if (strstr($faIcon, "fa-solid ")) $viewData['fasIcons'][] = array('val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false);
+  if (strstr($faIcon, "fa-brands ")) {
+    $viewData['fabIcons'][] = array( 'val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false );
+  }
+  if (strstr($faIcon, "fa-regular ")) {
+    $viewData['farIcons'][] = array( 'val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false );
+  }
+  if (strstr($faIcon, "fa-solid ")) {
+    $viewData['fasIcons'][] = array( 'val' => $faIcon, 'name' => proper($faIcon), 'selected' => ($AA->icon == $faIcon) ? true : false );
+  }
 }
 
 //=============================================================================
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
