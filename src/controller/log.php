f98b84fdad0980aa4ce57f42d663cbbc7cfc0ed7
George Lewe <george@lewe.com>
2024-07-25 18:42:05 +0200

    Sonar fixes (log controller)

--- src/controller/log.php
+++ src/controller/log.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Log Controller
  *
@@ -22,7 +24,7 @@ if (!isAllowed($CONF['controllers'][$controller]->permission)) {
   $alertData['subject'] = $LANG['alert_not_allowed_subject'];
   $alertData['text'] = $LANG['alert_not_allowed_text'];
   $alertData['help'] = $LANG['alert_not_allowed_help'];
-  require_once(WEBSITE_ROOT . '/controller/alert.php');
+  require_once WEBSITE_ROOT . '/controller/alert.php';
   die();
 }
 
@@ -49,10 +51,14 @@ $viewData['logPeriod'] = '';
 $viewData['logSearchUser'] = '';
 $viewData['logSearchEvent'] = '';
 $sort = "DESC";
-if (isset($_GET['sort']) && strtoupper($_GET['sort']) == "ASC") $sort = "ASC";
+if (isset($_GET['sort']) && strtoupper($_GET['sort']) == "ASC") {
+  $sort = "ASC";
+}
 $eventsPerPage = 50;
 $currentPage = 1;
-if (isset($_GET['page'])) $currentPage = intval(sanitize($_GET['page']));
+if (isset($_GET['page'])) {
+  $currentPage = intval(sanitize($_GET['page']));
+}
 
 $logToday = dateInfo(date("Y"), date("m"), date("d"));
 $C->save("logto", $logToday['ISO'] . ' 23:59:59.999999'); // Default is today
@@ -86,16 +92,13 @@ if (!empty($_POST)) {
   // Sanitize input
   //
   $_POST = sanitize($_POST);
-
   //
   // Form validation
   //
   $inputError = false;
-
   //
   // Validate input data. If something is wrong or missing, set $inputError = true
   //
-
   if (!$inputError) {
     // ,---------,
     // | Refresh |
@@ -196,20 +199,27 @@ if (!empty($_POST)) {
         //
         // Set log levels
         //
-        if (isset($_POST['chk_log' . $lt]) && $_POST['chk_log' . $lt]) $C->save("log" . $lt, "1");
-        else $C->save("log" . $lt, "0");
-
+        if (isset($_POST['chk_log' . $lt]) && $_POST['chk_log' . $lt]) {
+          $C->save("log" . $lt, "1");
+        } else {
+          $C->save("log" . $lt, "0");
+        }
         //
         // Set log filters
         //
-        if (isset($_POST['chk_logfilter' . $lt]) && $_POST['chk_logfilter' . $lt]) $C->save("logfilter" . $lt, "1");
-        else $C->save("logfilter" . $lt, "0");
-
+        if (isset($_POST['chk_logfilter' . $lt]) && $_POST['chk_logfilter' . $lt]) {
+          $C->save("logfilter" . $lt, "1");
+        } else {
+          $C->save("logfilter" . $lt, "0");
+        }
         //
         // Set log colors
         //
-        if ($_POST['opt_logcolor' . $lt]) $C->save("logcolor" . $lt, $_POST['opt_logcolor' . $lt]);
-        else $C->save("logcolor" . $lt, "default");
+        if ($_POST['opt_logcolor' . $lt]) {
+          $C->save("logcolor" . $lt, $_POST['opt_logcolor' . $lt]);
+        } else {
+          $C->save("logcolor" . $lt, "default");
+        }
       }
       //
       // Log this event
@@ -224,7 +234,6 @@ if (!empty($_POST)) {
       $periodFrom = $C->read("logfrom");
       $periodTo = $C->read("logto");
       $LOG->delete($periodFrom, $periodTo);
-
       //
       // Log this event
       //
@@ -273,14 +282,22 @@ if (!empty($_POST)) {
 $periodFrom = $C->read("logfrom");
 $periodTo = $C->read("logto");
 $logPeriod = $C->read("logperiod");
-if (!$logType = $C->read("logtype")) $logType = '%';
-if (!$logSearchUser = $C->read("logsearchuser")) $logSearchUser = '%';
-if (!$logSearchEvent = $C->read("logsearchevent")) $logSearchEvent = '%';
+if (!$logType = $C->read("logtype")) {
+  $logType = '%';
+}
+if (!$logSearchUser = $C->read("logsearchuser")) {
+  $logSearchUser = '%';
+}
+if (!$logSearchEvent = $C->read("logsearchevent")) {
+  $logSearchEvent = '%';
+}
 $events = $LOG->read($sort, $periodFrom, $periodTo, $logType, $logSearchUser, $logSearchEvent);
 $viewData['events'] = array();
 if (count($events)) {
   foreach ($events as $event) {
-    if ($C->read("logfilter" . substr($event['type'], 3))) $viewData['events'][] = $event;
+    if ($C->read("logfilter" . substr($event['type'], 3))) {
+      $viewData['events'][] = $event;
+    }
   }
 }
 $viewData['types'] = $logtypes;
@@ -291,15 +308,18 @@ $viewData['logtype'] = $logType;
 $viewData['numEvents'] = count($viewData['events']);
 $viewData['eventsPerPage'] = $eventsPerPage;
 $viewData['sort'] = $sort;
-if ($viewData['numEvents']) $viewData['numPages'] = ceil($viewData['numEvents'] / $viewData['eventsPerPage']);
-else $viewData['numPages'] = 0;
+if ($viewData['numEvents']) {
+  $viewData['numPages'] = ceil($viewData['numEvents'] / $viewData['eventsPerPage']);
+} else {
+  $viewData['numPages'] = 0;
+}
 $viewData['currentPage'] = $currentPage;
 
 //=============================================================================
 //
 // SHOW VIEW
 //
-require_once(WEBSITE_ROOT . '/views/header.php');
-require_once(WEBSITE_ROOT . '/views/menu.php');
-include_once(WEBSITE_ROOT . '/views/' . $controller . '.php');
-require_once(WEBSITE_ROOT . '/views/footer.php');
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
