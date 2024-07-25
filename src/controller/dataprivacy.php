7845d94babd72ededc2bc1fb4c4172578698595a
George Lewe <george@lewe.com>
2024-07-25 17:14:49 +0200

    Sonar fixes (dataprivacy controller)

--- src/controller/dataprivacy.php
+++ src/controller/dataprivacy.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Data Privacy Controller
  *
@@ -17,31 +19,53 @@ if (!defined('VALID_ROOT')) { exit(''); }
 // CHECK ENABLED
 //
 if (!$C->read('gdprPolicyPage')) {
-    header("Location: index.php?action=home");
-    die();
+  header("Location: index.php?action=home");
+  die();
 }
 
 //
 // Load policy
 //
-require("languages/" . $language . ".gdpr.php");
+require_once "languages/" . $language . ".gdpr.php";
 
 $viewData['gdpr_text'] = str_replace("%ENTITY%", $C->read('gdprOrganization'), $LANG['gdpr_start']);
 $viewData['gdpr_text'] = str_replace("%CONTROLLER%", nl2br($C->read('gdprController')), $viewData['gdpr_text']);
 $viewData['gdpr_text'] = str_replace("%DATAPROTECTIONOFFICER%", nl2br($C->read('gdprOfficer')), $viewData['gdpr_text']);
 
 $sectionNbr = 11;
-if ($C->read('gdprFacebook')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_facebook'], $sectionNbr++);
-if ($C->read('gdprGoogleAnalytics')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_google_analytics'], $sectionNbr++);
-if ($C->read('gdprInstagram')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_instagram'], $sectionNbr++);
-if ($C->read('gdprLinkedin')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_linkedin'], $sectionNbr++);
-if ($C->read('gdprPaypal')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_paypal'], $sectionNbr++);
-if ($C->read('gdprPinterest')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_pinterest'], $sectionNbr++);
-if ($C->read('gdprSlideshare')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_slideshare'], $sectionNbr++);
-if ($C->read('gdprTumblr')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_tumblr'], $sectionNbr++);
-if ($C->read('gdprTwitter')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_twitter'], $sectionNbr++);
-if ($C->read('gdprXing')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_xing'], $sectionNbr++);
-if ($C->read('gdprYoutube')) $viewData['gdpr_text'] .= sprintf($LANG['gdpr_youtube'], $sectionNbr++);
+if ($C->read('gdprFacebook')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_facebook'], $sectionNbr++);
+}
+if ($C->read('gdprGoogleAnalytics')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_google_analytics'], $sectionNbr++);
+}
+if ($C->read('gdprInstagram')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_instagram'], $sectionNbr++);
+}
+if ($C->read('gdprLinkedin')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_linkedin'], $sectionNbr++);
+}
+if ($C->read('gdprPaypal')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_paypal'], $sectionNbr++);
+}
+if ($C->read('gdprPinterest')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_pinterest'], $sectionNbr++);
+}
+if ($C->read('gdprSlideshare')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_slideshare'], $sectionNbr++);
+}
+if ($C->read('gdprTumblr')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_tumblr'], $sectionNbr++);
+}
+if ($C->read('gdprTwitter')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_twitter'], $sectionNbr++);
+}
+if ($C->read('gdprXing')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_xing'], $sectionNbr++);
+}
+if ($C->read('gdprYoutube')) {
+  $viewData['gdpr_text'] .= sprintf($LANG['gdpr_youtube'], $sectionNbr++);
+}
 
 $viewData['gdpr_text'] .= sprintf($LANG['gdpr_end'], $sectionNbr++, $sectionNbr++, $sectionNbr++, $sectionNbr++, $sectionNbr++);
 
@@ -49,7 +73,7 @@ $viewData['gdpr_text'] .= sprintf($LANG['gdpr_end'], $sectionNbr++, $sectionNbr+
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
