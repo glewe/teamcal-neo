673a663725ef948f0b8e296c8ef93045b2838a64
George Lewe <george@lewe.com>
2024-07-25 18:30:33 +0200

    Sonar fixes (groups controller)

--- src/controller/groups.php
+++ src/controller/groups.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Groups Controller
  *
@@ -12,20 +14,18 @@ if (!defined('VALID_ROOT')) { exit(''); }
  * @since 3.0.0
  */
 
-// echo '<script type="text/javascript">alert("Debug: ");</script>';
-
 //=============================================================================
 //
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission)) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -35,7 +35,7 @@ if (!isAllowed($CONF['controllers'][$controller]->permission)) {
 $alertData = array();
 $showAlert = false;
 $licExpiryWarning = $C->read('licExpiryWarning');
-$LIC  = new License();
+$LIC = new License();
 $LIC->check($alertData, $showAlert, $licExpiryWarning, $LANG);
 
 //=============================================================================
@@ -55,124 +55,124 @@ $viewData['txt_description'] = '';
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    //
-    // Sanitize input
-    //
-    $_POST = sanitize($_POST);
+  //
+  // Sanitize input
+  //
+  $_POST = sanitize($_POST);
+  //
+  // Form validation
+  //
+  $inputError = false;
+  //
+  // Validate input data. If something is wrong or missing, set $inputError = true
+  //
+  if (!$inputError) {
+    // ,--------,
+    // | Create |
+    // '--------'
+    if (isset($_POST['btn_groupCreate'])) {
+      //
+      // Sanitize input
+      //
+      $_POST = sanitize($_POST);
+      //
+      // Form validation
+      //
+      $inputAlert = array();
+      $inputError = false;
+      if (!formInputValid('txt_name', 'required|alpha_numeric_dash')) {
+        $inputError = true;
+      }
+      if (!formInputValid('txt_description', 'alpha_numeric_dash_blank')) {
+        $inputError = true;
+      }
 
-    //
-    // Form validation
-    //
-    $inputError = false;
+      $viewData['txt_name'] = $_POST['txt_name'];
+      if (isset($_POST['txt_description'])) $viewData['txt_description'] = $_POST['txt_description'];
 
-    //
-    // Validate input data. If something is wrong or missing, set $inputError = true
-    //
+      if (!$inputError) {
+        $G->name = $viewData['txt_name'];
+        $G->description = $viewData['txt_description'];
+        $G->minpresent = 0;
+        $G->maxabsent = 9999;
+        $G->minpresentwe = 0;
+        $G->maxabsentwe = 9999;
+        $G->create();
 
-    if (!$inputError) {
-        // ,--------,
-        // | Create |
-        // '--------'
-        if (isset($_POST['btn_groupCreate'])) {
-            //
-            // Sanitize input
-            //
-            $_POST = sanitize($_POST);
-
-            //
-            // Form validation
-            //
-            $inputAlert = array();
-            $inputError = false;
-            if (!formInputValid('txt_name', 'required|alpha_numeric_dash')) $inputError = true;
-            if (!formInputValid('txt_description', 'alpha_numeric_dash_blank')) $inputError = true;
-
-            $viewData['txt_name'] = $_POST['txt_name'];
-            if (isset($_POST['txt_description'])) $viewData['txt_description'] = $_POST['txt_description'];
-
-            if (!$inputError) {
-                $G->name = $viewData['txt_name'];
-                $G->description = $viewData['txt_description'];
-                $G->minpresent = 0;
-                $G->maxabsent = 9999;
-                $G->minpresentwe = 0;
-                $G->maxabsentwe = 9999;
-                $G->create();
-
-                //
-                // Send notification e-mails to the subscribers of group events
-                //
-                if ($C->read("emailNotifications")) {
-                    sendGroupEventNotifications("created", $G->name, $G->description);
-                }
-
-                //
-                // Log this event
-                //
-                $LOG->logEvent("logGroup", L_USER, "log_group_created", $G->name . " " . $G->description);
-
-                //
-                // Success
-                //
-                $showAlert =  true;
-                $alertData['type'] = 'success';
-                $alertData['title'] = $LANG['alert_success_title'];
-                $alertData['subject'] = $LANG['btn_create_group'];
-                $alertData['text'] = $LANG['groups_alert_group_created'];
-                $alertData['help'] = '';
-            } else {
-                //
-                // Fail
-                //
-                $showAlert =  true;
-                $alertData['type'] = 'danger';
-                $alertData['title'] = $LANG['alert_danger_title'];
-                $alertData['subject'] = $LANG['btn_create_group'];
-                $alertData['text'] = $LANG['groups_alert_group_created_fail'];
-                $alertData['help'] = '';
-            }
-        }
-        // ,--------,
-        // | Delete |
-        // '--------'
-        elseif (isset($_POST['btn_groupDelete'])) {
-            $G->delete($_POST['hidden_id']);
-            $UG->deleteByGroup($_POST['hidden_id']);
-            $UO->deleteOptionByValue('calfilterGroup', $_POST['hidden_id']);
-
-            //
-            // Send notification e-mails to the subscribers of group events
-            //
-            if ($C->read("emailNotifications")) {
-                sendGroupEventNotifications("deleted", $_POST['hidden_name'], $_POST['hidden_description']);
-            }
-
-            //
-            // Log this event
-            //
-            $LOG->logEvent("logGroup", L_USER, "log_group_deleted", $_POST['hidden_name']);
-
-            //
-            // Success
-            //
-            $showAlert =  true;
-            $alertData['type'] = 'success';
-            $alertData['title'] = $LANG['alert_success_title'];
-            $alertData['subject'] = $LANG['btn_delete_group'];
-            $alertData['text'] = $LANG['groups_alert_group_deleted'];
-            $alertData['help'] = '';
-        }
-    } else {
         //
-        // Input validation failed
+        // Send notification e-mails to the subscribers of group events
         //
-        $showAlert =  true;
+        if ($C->read("emailNotifications")) {
+          sendGroupEventNotifications("created", $G->name, $G->description);
+        }
+
+        //
+        // Log this event
+        //
+        $LOG->logEvent("logGroup", L_USER, "log_group_created", $G->name . " " . $G->description);
+
+        //
+        // Success
+        //
+        $showAlert = true;
+        $alertData['type'] = 'success';
+        $alertData['title'] = $LANG['alert_success_title'];
+        $alertData['subject'] = $LANG['btn_create_group'];
+        $alertData['text'] = $LANG['groups_alert_group_created'];
+        $alertData['help'] = '';
+      } else {
+        //
+        // Fail
+        //
+        $showAlert = true;
         $alertData['type'] = 'danger';
         $alertData['title'] = $LANG['alert_danger_title'];
-        $alertData['subject'] = $LANG['alert_input'];
-        $alertData['text'] = $LANG['register_alert_failed'];
+        $alertData['subject'] = $LANG['btn_create_group'];
+        $alertData['text'] = $LANG['groups_alert_group_created_fail'];
         $alertData['help'] = '';
+      }
     }
+    // ,--------,
+    // | Delete |
+    // '--------'
+    elseif (isset($_POST['btn_groupDelete'])) {
+      $G->delete($_POST['hidden_id']);
+      $UG->deleteByGroup($_POST['hidden_id']);
+      $UO->deleteOptionByValue('calfilterGroup', $_POST['hidden_id']);
+
+      //
+      // Send notification e-mails to the subscribers of group events
+      //
+      if ($C->read("emailNotifications")) {
+        sendGroupEventNotifications("deleted", $_POST['hidden_name'], $_POST['hidden_description']);
+      }
+
+      //
+      // Log this event
+      //
+      $LOG->logEvent("logGroup", L_USER, "log_group_deleted", $_POST['hidden_name']);
+
+      //
+      // Success
+      //
+      $showAlert = true;
+      $alertData['type'] = 'success';
+      $alertData['title'] = $LANG['alert_success_title'];
+      $alertData['subject'] = $LANG['btn_delete_group'];
+      $alertData['text'] = $LANG['groups_alert_group_deleted'];
+      $alertData['help'] = '';
+    }
+  } else {
+    //
+    // Input validation failed
+    //
+    $showAlert = true;
+    $alertData['type'] = 'danger';
+    $alertData['title'] = $LANG['alert_danger_title'];
+    $alertData['subject'] = $LANG['alert_input'];
+    $alertData['text'] = $LANG['register_alert_failed'];
+    $alertData['help'] = '';
+  }
 }
 
 //=============================================================================
@@ -192,20 +192,19 @@ $viewData['searchGroup'] = '';
 // Adjust users by requested search
 //
 if (isset($_POST['btn_search'])) {
-    $searchUsers = array();
-
-    if (isset($_POST['txt_searchGroup'])) {
-        $searchGroup = sanitize($_POST['txt_searchGroup']);
-        $viewData['searchGroup'] = $searchGroup;
-        $viewData['groups'] = $G->getAllLike($searchGroup);
-    }
+  $searchUsers = array();
+  if (isset($_POST['txt_searchGroup'])) {
+    $searchGroup = sanitize($_POST['txt_searchGroup']);
+    $viewData['searchGroup'] = $searchGroup;
+    $viewData['groups'] = $G->getAllLike($searchGroup);
+  }
 }
 
 //=============================================================================
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
