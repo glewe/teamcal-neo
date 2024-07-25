c5d21cb629a0c24f9aec744b78cd62d7f6b02ea3
George Lewe <george@lewe.com>
2024-07-25 18:51:34 +0200

    Sonar fixes (messages controller)

--- src/controller/messages.php
+++ src/controller/messages.php
@@ -1,4 +1,7 @@
 <?php
+if (!defined('VALID_ROOT')) {
+  exit('No direct access allowed!');
+}
 
 /**
  * Messages page controller
@@ -11,22 +14,19 @@
  * @subpackage Controllers
  * @since 3.0.0
  */
-if (!defined('VALID_ROOT')) exit('No direct access allowed!');
-
-// echo '<script type="text/javascript">alert("Debug: ");</script>';
 
 //=============================================================================
 //
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission) || !$C->read('activateMessages')) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -45,79 +45,73 @@ $msgData = $MSG->getAllByUser($UL->username);
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    //
-    // Sanitize input
-    //
-    $_POST = sanitize($_POST);
+  //
+  // Sanitize input
+  //
+  $_POST = sanitize($_POST);
+  //
+  // Form validation
+  //
+  $inputError = false;
+  //
+  // Validate input data. If something is wrong or missing, set $inputError = true
+  //
 
-    //
-    // Form validation
-    //
-    $inputError = false;
-
-    //
-    // Validate input data. If something is wrong or missing, set $inputError = true
-    //
-
-    if (!$inputError) {
-        // ,---------,
-        // | Confirm |
-        // '---------'
-        if (isset($_POST['btn_confirm'])) {
-            $UMSG->setSilent($_POST['msgId']);
-
-            $LOG->logEvent("logMessages", $UL->username, "log_msg_confirmed", $_POST['msgId']);
-            header("Location: index.php?action=" . $controller);
-        }
-        // ,-------------,
-        // | Confirm All |
-        // '-------------'
-        else if (isset($_POST['btn_confirm_all'])) {
-            $UMSG->setSilentByUser($UL->username);
-
-            //
-            // Log this event
-            //
-            $LOG->logEvent("logMessages", $UL->username, "log_msg_all_confirmed_by", $UL->username);
-            header("Location: index.php?action=" . $controller);
-        }
-        // ,--------,
-        // | Delete |
-        // '--------'
-        else if (isset($_POST['btn_delete'])) {
-            $UMSG->delete($_POST['msgId']);
-            deleteOrphanedMessages();
-
-            //
-            // Log this event
-            //
-            $LOG->logEvent("logMessages", $UL->username, "log_msg_deleted", $_POST['msgId']);
-            header("Location: index.php?action=" . $controller);
-        }
-        // ,------------,
-        // | Delete All |
-        // '------------'
-        else if (isset($_POST['btn_delete_all'])) {
-            $UMSG->deleteByUser($UL->username);
-            deleteOrphanedMessages();
-
-            //
-            // Log this event
-            //
-            $LOG->logEvent("logMessages", $UL->username, "log_msg_all_deleted", $UL->username);
-            header("Location: index.php?action=" . $controller);
-        }
-    } else {
-        //
-        // Input validation failed
-        //
-        $showAlert =  true;
-        $alertData['type'] = 'danger';
-        $alertData['title'] = $LANG['alert_danger_title'];
-        $alertData['subject'] = $LANG['alert_input'];
-        $alertData['text'] = $LANG['register_alert_failed'];
-        $alertData['help'] = '';
+  if (!$inputError) {
+    // ,---------,
+    // | Confirm |
+    // '---------'
+    if (isset($_POST['btn_confirm'])) {
+      $UMSG->setSilent($_POST['msgId']);
+      $LOG->logEvent("logMessages", $UL->username, "log_msg_confirmed", $_POST['msgId']);
+      header("Location: index.php?action=" . $controller);
     }
+    // ,-------------,
+    // | Confirm All |
+    // '-------------'
+    else if (isset($_POST['btn_confirm_all'])) {
+      $UMSG->setSilentByUser($UL->username);
+      //
+      // Log this event
+      //
+      $LOG->logEvent("logMessages", $UL->username, "log_msg_all_confirmed_by", $UL->username);
+      header("Location: index.php?action=" . $controller);
+    }
+    // ,--------,
+    // | Delete |
+    // '--------'
+    else if (isset($_POST['btn_delete'])) {
+      $UMSG->delete($_POST['msgId']);
+      deleteOrphanedMessages();
+      //
+      // Log this event
+      //
+      $LOG->logEvent("logMessages", $UL->username, "log_msg_deleted", $_POST['msgId']);
+      header("Location: index.php?action=" . $controller);
+    }
+    // ,------------,
+    // | Delete All |
+    // '------------'
+    else if (isset($_POST['btn_delete_all'])) {
+      $UMSG->deleteByUser($UL->username);
+      deleteOrphanedMessages();
+      //
+      // Log this event
+      //
+      $LOG->logEvent("logMessages", $UL->username, "log_msg_all_deleted", $UL->username);
+      header("Location: index.php?action=" . $controller);
+    }
+  } else {
+    //
+    // Input validation failed
+    //
+    $showAlert = true;
+    $alertData['type'] = 'danger';
+    $alertData['title'] = $LANG['alert_danger_title'];
+    $alertData['subject'] = $LANG['alert_input'];
+    $alertData['text'] = $LANG['register_alert_failed'];
+    $alertData['help'] = '';
+  }
 }
 
 //=============================================================================
@@ -129,7 +123,7 @@ if (!empty($_POST)) {
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
