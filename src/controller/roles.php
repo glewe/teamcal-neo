3e7a1d6a05b091799de9fea2cc0de9906ad36936
George Lewe <george@lewe.com>
2024-07-26 08:31:38 +0200

    Sonar fixes (roles controller)

--- src/controller/roles.php
+++ src/controller/roles.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Roles Controller
  *
@@ -12,20 +14,18 @@ if (!defined('VALID_ROOT')) { exit(''); }
  * @since 3.0.0
  */
 
-// echo '<script type="text/javascript">alert("Debug: ");</script>';
-
 //=============================================================================
 //
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission)) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -35,7 +35,7 @@ if (!isAllowed($CONF['controllers'][$controller]->permission)) {
 $alertData = array();
 $showAlert = false;
 $licExpiryWarning = $C->read('licExpiryWarning');
-$LIC  = new License();
+$LIC = new License();
 $LIC->check($alertData, $showAlert, $licExpiryWarning, $LANG);
 
 //=============================================================================
@@ -55,136 +55,127 @@ $viewData['txt_description'] = '';
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    //
-    // Sanitize input
-    //
-    $_POST = sanitize($_POST);
+  //
+  // Sanitize input
+  //
+  $_POST = sanitize($_POST);
+  //
+  // Form validation
+  //
+  $inputError = false;
+  //
+  // Validate input data. If something is wrong or missing, set $inputError = true
+  //
+  if (!$inputError) {
+    // ,--------,
+    // | Create |
+    // '--------'
+    if (isset($_POST['btn_roleCreate'])) {
+      //
+      // Sanitize input
+      //
+      $_POST = sanitize($_POST);
+      //
+      // Form validation
+      //
+      $inputAlert = array();
+      $inputError = false;
+      if (
+        !formInputValid('txt_name', 'required|alpha_numeric_dash') ||
+        !formInputValid('txt_description', 'alpha_numeric_dash_blank')
+      ) {
+        $inputError = true;
+        $alertData['text'] = $LANG['roles_alert_created_fail_input'];
+      }
+      $viewData['txt_name'] = $_POST['txt_name'];
 
-    //
-    // Form validation
-    //
-    $inputError = false;
+      if (isset($_POST['txt_description'])) {
+        $viewData['txt_description'] = $_POST['txt_description'];
+      }
 
-    //
-    // Validate input data. If something is wrong or missing, set $inputError = true
-    //
+      if ($RO->getByName($_POST['txt_name'])) {
+        $inputError = true;
+        $alertData['text'] = $LANG['roles_alert_created_fail_duplicate'];
+      }
 
-    if (!$inputError) {
-        // ,--------,
-        // | Create |
-        // '--------'
-        if (isset($_POST['btn_roleCreate'])) {
-            //
-            // Sanitize input
-            //
-            $_POST = sanitize($_POST);
-
-            //
-            // Form validation
-            //
-            $inputAlert = array();
-            $inputError = false;
-            if (
-                !formInputValid('txt_name', 'required|alpha_numeric_dash') ||
-                !formInputValid('txt_description', 'alpha_numeric_dash_blank')
-            ) {
-                $inputError = true;
-                $alertData['text'] = $LANG['roles_alert_created_fail_input'];
-            }
-
-            $viewData['txt_name'] = $_POST['txt_name'];
-            if (isset($_POST['txt_description'])) $viewData['txt_description'] = $_POST['txt_description'];
-
-            if ($RO->getByName($_POST['txt_name'])) {
-                $inputError = true;
-                $alertData['text'] = $LANG['roles_alert_created_fail_duplicate'];
-            }
-
-            if (!$inputError) {
-                $RO->name = $viewData['txt_name'];
-                $RO->description = $viewData['txt_description'];
-                $RO->color = 'default';
-                $RO->create();
-
-                //
-                // Send notification e-mails to the subscribers of role events
-                //
-                if ($C->read("emailNotifications")) {
-                    sendRoleEventNotifications("created", $RO->name, $RO->description);
-                }
-
-                //
-                // Log this event
-                //
-                $LOG->logEvent("logRole", L_USER, "log_role_created", $RO->name . " " . $RO->description);
-
-                //
-                // Success
-                //
-                $showAlert =  true;
-                $alertData['type'] = 'success';
-                $alertData['title'] = $LANG['alert_success_title'];
-                $alertData['subject'] = $LANG['btn_create_role'];
-                $alertData['text'] = $LANG['roles_alert_created'];
-                $alertData['help'] = '';
-            } else {
-                //
-                // Fail
-                //
-                $showAlert =  true;
-                $alertData['type'] = 'danger';
-                $alertData['title'] = $LANG['alert_danger_title'];
-                $alertData['subject'] = $LANG['btn_create_role'];
-                $alertData['help'] = '';
-            }
-        }
-        // ,--------,
-        // | Delete |
-        // '--------'
-        elseif (isset($_POST['btn_roleDelete'])) {
-            //
-            // Delete Role
-            //
-            $RO->delete($_POST['hidden_id']);
-
-            //
-            // Delete Role in all permission schemes
-            //
-            $P->deleteRole($_POST['hidden_id']);
-
-            //
-            // Send notification e-mails to the subscribers of role events
-            //
-            if ($C->read("emailNotifications")) {
-                sendRoleEventNotifications("deleted", $_POST['hidden_name'], $_POST['hidden_description']);
-            }
-
-            //
-            // Log this event
-            //
-            $LOG->logEvent("logRole", L_USER, "log_role_deleted", $_POST['hidden_name']);
-
-            //
-            // Success
-            //
-            $showAlert =  true;
-            $alertData['type'] = 'success';
-            $alertData['title'] = $LANG['alert_success_title'];
-            $alertData['subject'] = $LANG['btn_delete_role'];
-            $alertData['text'] = $LANG['roles_alert_deleted'];
-            $alertData['help'] = '';
-        }
-    } else {
+      if (!$inputError) {
+        $RO->name = $viewData['txt_name'];
+        $RO->description = $viewData['txt_description'];
+        $RO->color = 'default';
+        $RO->create();
         //
-        // Input validation failed
+        // Send notification e-mails to the subscribers of role events
         //
-        $showAlert =  true;
+        if ($C->read("emailNotifications")) {
+          sendRoleEventNotifications("created", $RO->name, $RO->description);
+        }
+        //
+        // Log this event
+        //
+        $LOG->logEvent("logRole", L_USER, "log_role_created", $RO->name . " " . $RO->description);
+        //
+        // Success
+        //
+        $showAlert = true;
+        $alertData['type'] = 'success';
+        $alertData['title'] = $LANG['alert_success_title'];
+        $alertData['subject'] = $LANG['btn_create_role'];
+        $alertData['text'] = $LANG['roles_alert_created'];
+        $alertData['help'] = '';
+      } else {
+        //
+        // Fail
+        //
+        $showAlert = true;
         $alertData['type'] = 'danger';
         $alertData['title'] = $LANG['alert_danger_title'];
-        $alertData['subject'] = $LANG['alert_input'];
-        $alertData['text'] = $LANG['register_alert_failed'];
+        $alertData['subject'] = $LANG['btn_create_role'];
         $alertData['help'] = '';
+      }
     }
+    // ,--------,
+    // | Delete |
+    // '--------'
+    elseif (isset($_POST['btn_roleDelete'])) {
+      //
+      // Delete Role
+      //
+      $RO->delete($_POST['hidden_id']);
+      //
+      // Delete Role in all permission schemes
+      //
+      $P->deleteRole($_POST['hidden_id']);
+      //
+      // Send notification e-mails to the subscribers of role events
+      //
+      if ($C->read("emailNotifications")) {
+        sendRoleEventNotifications("deleted", $_POST['hidden_name'], $_POST['hidden_description']);
+      }
+      //
+      // Log this event
+      //
+      $LOG->logEvent("logRole", L_USER, "log_role_deleted", $_POST['hidden_name']);
+      //
+      // Success
+      //
+      $showAlert = true;
+      $alertData['type'] = 'success';
+      $alertData['title'] = $LANG['alert_success_title'];
+      $alertData['subject'] = $LANG['btn_delete_role'];
+      $alertData['text'] = $LANG['roles_alert_deleted'];
+      $alertData['help'] = '';
+    }
+  } else {
+    //
+    // Input validation failed
+    //
+    $showAlert = true;
+    $alertData['type'] = 'danger';
+    $alertData['title'] = $LANG['alert_danger_title'];
+    $alertData['subject'] = $LANG['alert_input'];
+    $alertData['text'] = $LANG['register_alert_failed'];
+    $alertData['help'] = '';
+  }
 }
 
 //=============================================================================
@@ -202,20 +193,19 @@ $viewData['searchRole'] = '';
 // | Search |
 // '--------'
 if (isset($_POST['btn_search'])) {
-    $searchUsers = array();
-
-    if (isset($_POST['txt_searchRole'])) {
-        $searchRole = sanitize($_POST['txt_searchRole']);
-        $viewData['searchRole'] = $searchRole;
-        $viewData['roles'] = $RO->getAllLike($searchRole);
-    }
+  $searchUsers = array();
+  if (isset($_POST['txt_searchRole'])) {
+    $searchRole = sanitize($_POST['txt_searchRole']);
+    $viewData['searchRole'] = $searchRole;
+    $viewData['roles'] = $RO->getAllLike($searchRole);
+  }
 }
 
 //=============================================================================
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
