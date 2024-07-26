8352e4545b3e386f784ab7c10ee35c979fdb445a
George Lewe <george@lewe.com>
2024-07-26 18:41:52 +0200

    Sonar fixes (year controller)

--- src/controller/year.php
+++ src/controller/year.php
@@ -14,50 +14,53 @@ if (!defined('VALID_ROOT')) {
  * @since 3.0.0
  */
 
-// echo '<script type="text/javascript">alert("Debug: ");</script>';
-
 //=============================================================================
 //
 // CHECK URL PARAMETERS
 //
 if (isset($_GET['year']) && isset($_GET['region']) && isset($_GET['user'])) {
-    $missingData =  false;
+  $missingData = false;
+  $yyyy = sanitize($_GET['year']);
+  if (!is_numeric($yyyy) || strlen($yyyy) != 4 || !checkdate(1, 1, intval($yyyy))) {
+    $missingData = true;
+  }
 
-    $yyyy = sanitize($_GET['year']);
-    if (!is_numeric($yyyy) || strlen($yyyy) != 4 || !checkdate(1, 1, intval($yyyy))) $missingData =  true;
+  $region = sanitize($_GET['region']);
+  if (!$R->getById($region)) {
+    $missingData = true;
+  }
 
-    $region = sanitize($_GET['region']);
-    if (!$R->getById($region)) $missingData =  true;
-
-    if (strlen($_GET['user'])) {
-        $user = sanitize($_GET['user']);
-        if (!$U->exists($user)) $missingData =  true;
-    } else {
-        $user = '';
+  if (strlen($_GET['user'])) {
+    $user = sanitize($_GET['user']);
+    if (!$U->exists($user)) {
+      $missingData = true;
     }
+  } else {
+    $user = '';
+  }
 } else {
-    $missingData =  true;
+  $missingData = true;
 }
 
 if ($missingData) {
-    //
-    // URL param fail
-    //
-    $alertData['type'] = 'danger';
-    $alertData['title'] = $LANG['alert_danger_title'];
-    $alertData['subject'] = $LANG['alert_no_data_subject'];
-    $alertData['text'] = $LANG['alert_no_data_text'];
-    $alertData['help'] = $LANG['alert_no_data_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  //
+  // URL param fail
+  //
+  $alertData['type'] = 'danger';
+  $alertData['title'] = $LANG['alert_danger_title'];
+  $alertData['subject'] = $LANG['alert_no_data_subject'];
+  $alertData['text'] = $LANG['alert_no_data_text'];
+  $alertData['help'] = $LANG['alert_no_data_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //
 // Default back to current yearmonth if option is set
 //
 if ($C->read('currentYearOnly') && $yyyy != date('Y')) {
-    header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . date('Y') . "&region=" . $region . "&user=" . $user);
-    die();
+  header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . date('Y') . "&region=" . $region . "&user=" . $user);
+  die();
 }
 
 //=============================================================================
@@ -65,13 +68,13 @@ if ($C->read('currentYearOnly') && $yyyy != date('Y')) {
 // CHECK PERMISSION
 //
 if (!isAllowed($CONF['controllers'][$controller]->permission)) {
-    $alertData['type'] = 'warning';
-    $alertData['title'] = $LANG['alert_alert_title'];
-    $alertData['subject'] = $LANG['alert_not_allowed_subject'];
-    $alertData['text'] = $LANG['alert_not_allowed_text'];
-    $alertData['help'] = $LANG['alert_not_allowed_help'];
-    require(WEBSITE_ROOT . '/controller/alert.php');
-    die();
+  $alertData['type'] = 'warning';
+  $alertData['title'] = $LANG['alert_alert_title'];
+  $alertData['subject'] = $LANG['alert_not_allowed_subject'];
+  $alertData['text'] = $LANG['alert_not_allowed_text'];
+  $alertData['help'] = $LANG['alert_not_allowed_help'];
+  require_once WEBSITE_ROOT . '/controller/alert.php';
+  die();
 }
 
 //=============================================================================
@@ -91,93 +94,85 @@ $users = $U->getAll();
 // Loop through all months
 //
 for ($i = 1; $i <= 12; $i++) {
+  //
+  // See if a template for the month exists. If not, create one.
+  //
+  if (!$M->getMonth($yyyy, $i, $R->id)) {
+    createMonth($yyyy, $i, 'region', $R->id);
     //
-    // See if a template for the month exists. If not, create one.
+    // Send notification e-mails to the subscribers of user events
     //
-    if (!$M->getMonth($yyyy, $i, $R->id)) {
-        createMonth($yyyy, $i, 'region', $R->id);
-
-        //
-        // Send notification e-mails to the subscribers of user events
-        //
-        if ($C->read("emailNotifications")) {
-            sendMonthEventNotifications("created", $yyyy, $i, $R->name);
-        }
-
-        //
-        // Log this event
-        //
-        $LOG->logEvent("logMonth", L_USER, "log_month_tpl_created", $M->region . ": " . $M->year . "-" . $M->month);
+    if ($C->read("emailNotifications")) {
+      sendMonthEventNotifications("created", $yyyy, $i, $R->name);
     }
-
     //
-    // See if a template for the month for the user exists. If not, create one.
+    // Log this event
     //
-    if (!$T->getTemplate($user, $yyyy, $i)) {
-        createMonth($yyyy, $i, 'user', $user);
+    $LOG->logEvent("logMonth", L_USER, "log_month_tpl_created", $M->region . ": " . $M->year . "-" . $M->month);
+  }
+  //
+  // See if a template for the month for the user exists. If not, create one.
+  //
+  if (!$T->getTemplate($user, $yyyy, $i)) {
+    createMonth($yyyy, $i, 'user', $user);
+  }
+  $viewData['monthInfo'][$i] = dateInfo($yyyy, $i);
+  //
+  // Loop through all days of the current month
+  //
+  for ($d = 1; $d <= $viewData['monthInfo'][$i]['daysInMonth']; $d++) {
+    $viewData['month'][$i][$d]['wday'] = $M->getWeekday($yyyy, $i, $d, $R->id);
+    $viewData['month'][$i][$d]['hol'] = $M->getHoliday($yyyy, $i, $d, $R->id);
+    $viewData['month'][$i][$d]['abs'] = $T->getAbsence($user, $yyyy, $i, $d);
+    $viewData['month'][$i][$d]['symbol'] = '';
+    $viewData['month'][$i][$d]['icon'] = '';
+    $viewData['month'][$i][$d]['style'] = '';
+    $viewData['month'][$i][$d]['absstyle'] = '';
+    $color = '';
+    $bgcolor = '';
+    $border = '';
+    //
+    // Get weekend style
+    //
+    if ($viewData['month'][$i][$d]['wday'] == 6 || $viewData['month'][$i][$d]['wday'] == 7) {
+      $color = 'color: #' . $H->getColor($viewData['month'][$i][$d]['wday'] - 4) . ';';
+      $bgcolor = 'background-color: #' . $H->getBgColor($viewData['month'][$i][$d]['wday'] - 4) . ';';
     }
-
-    $viewData['monthInfo'][$i] = dateInfo($yyyy, $i);
-
     //
-    // Loop through all days of the current month
+    // Get holiday style (overwrites weekend style)
     //
-    for ($d = 1; $d <= $viewData['monthInfo'][$i]['daysInMonth']; $d++) {
-        $viewData['month'][$i][$d]['wday'] = $M->getWeekday($yyyy, $i, $d, $R->id);
-        $viewData['month'][$i][$d]['hol'] = $M->getHoliday($yyyy, $i, $d, $R->id);
-        $viewData['month'][$i][$d]['abs'] = $T->getAbsence($user, $yyyy, $i, $d);
-        $viewData['month'][$i][$d]['symbol'] = '';
-        $viewData['month'][$i][$d]['icon'] = '';
-        $viewData['month'][$i][$d]['style'] = '';
-        $viewData['month'][$i][$d]['absstyle'] = '';
-
-        $color = '';
-        $bgcolor = '';
-        $border = '';
-
-        //
-        // Get weekend style
-        //
-        if ($viewData['month'][$i][$d]['wday'] == 6 || $viewData['month'][$i][$d]['wday'] == 7) {
-            $color = 'color: #' . $H->getColor($viewData['month'][$i][$d]['wday'] - 4) . ';';
-            $bgcolor = 'background-color: #' . $H->getBgColor($viewData['month'][$i][$d]['wday'] - 4) . ';';
-        }
-
-        //
-        // Get holiday style (overwrites weekend style)
-        //
-        if ($viewData['month'][$i][$d]['hol']) {
-            $color = 'color: #' . $H->getColor($viewData['month'][$i][$d]['hol']) . ';';
-            $bgcolor = 'background-color: #' . $H->getBgColor($viewData['month'][$i][$d]['hol']) . ';';
-        }
-
-        //
-        // Get today style
-        //
-        $loopDate = date('Y-m-d', mktime(0, 0, 0, $i, $d, $yyyy));
-        if ($loopDate == $currDate) {
-            $border = 'border: ' . $C->read("todayBorderSize") . 'px solid #' . $C->read("todayBorderColor") . ';';
-        }
-
-        //
-        // Build styles
-        //
-        if (strlen($color) || strlen($bgcolor) || strlen($border)) {
-            $viewData['month'][$i][$d]['style'] = ' style="' . $color . $bgcolor . $border . '"';
-        }
-
-        //
-        // Get absence style of user template.
-        //
-        if ($viewData['month'][$i][$d]['abs']) {
-            $A->get($viewData['month'][$i][$d]['abs']);
-            $viewData['month'][$i][$d]['icon'] = $A->icon;
-            $viewData['month'][$i][$d]['symbol'] = $A->symbol;
-            if ($A->bgtrans) $bgStyle = "";
-            else $bgStyle = "background-color: #" . $A->bgcolor . ";";
-            $viewData['month'][$i][$d]['absstyle'] = ' style="color: #' . $A->color . ';' . $bgStyle . '"';
-        }
+    if ($viewData['month'][$i][$d]['hol']) {
+      $color = 'color: #' . $H->getColor($viewData['month'][$i][$d]['hol']) . ';';
+      $bgcolor = 'background-color: #' . $H->getBgColor($viewData['month'][$i][$d]['hol']) . ';';
     }
+    //
+    // Get today style
+    //
+    $loopDate = date('Y-m-d', mktime(0, 0, 0, $i, $d, $yyyy));
+    if ($loopDate == $currDate) {
+      $border = 'border: ' . $C->read("todayBorderSize") . 'px solid #' . $C->read("todayBorderColor") . ';';
+    }
+    //
+    // Build styles
+    //
+    if (strlen($color) || strlen($bgcolor) || strlen($border)) {
+      $viewData['month'][$i][$d]['style'] = ' style="' . $color . $bgcolor . $border . '"';
+    }
+    //
+    // Get absence style of user template.
+    //
+    if ($viewData['month'][$i][$d]['abs']) {
+      $A->get($viewData['month'][$i][$d]['abs']);
+      $viewData['month'][$i][$d]['icon'] = $A->icon;
+      $viewData['month'][$i][$d]['symbol'] = $A->symbol;
+      if ($A->bgtrans) {
+        $bgStyle = "";
+      } else {
+        $bgStyle = "background-color: #" . $A->bgcolor . ";";
+      }
+      $viewData['month'][$i][$d]['absstyle'] = ' style="color: #' . $A->color . ';' . $bgStyle . '"';
+    }
+  }
 }
 
 //=============================================================================
@@ -185,20 +180,20 @@ for ($i = 1; $i <= 12; $i++) {
 // PROCESS FORM
 //
 if (!empty($_POST)) {
-    // ,---------------,
-    // | Select Region |
-    // '---------------'
-    if (isset($_POST['btn_region'])) {
-        header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . $yyyy . "&region=" . $_POST['sel_region'] . "&user=" . $user);
-        die();
-    }
-    // ,---------------,
-    // | Select User   |
-    // '---------------'
-    elseif (isset($_POST['btn_user'])) {
-        header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . $yyyy . "&region=" . $region . "&user=" . $_POST['sel_user']);
-        die();
-    }
+  // ,---------------,
+  // | Select Region |
+  // '---------------'
+  if (isset($_POST['btn_region'])) {
+    header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . $yyyy . "&region=" . $_POST['sel_region'] . "&user=" . $user);
+    die();
+  }
+  // ,---------------,
+  // | Select User   |
+  // '---------------'
+  elseif (isset($_POST['btn_user'])) {
+    header("Location: " . $_SERVER['PHP_SELF'] . "?action=" . $controller . "&year=" . $yyyy . "&region=" . $region . "&user=" . $_POST['sel_user']);
+    die();
+  }
 }
 
 //=============================================================================
@@ -211,28 +206,23 @@ $viewData['year'] = $yyyy;
 $viewData['regionid'] = $R->id;
 $viewData['regionname'] = $R->name;
 $viewData['regions'] = $R->getAll();
-
 $viewData['users'] = array();
 foreach ($users as $usr) {
-    $allowed = false;
-    if ($usr['username'] == $UL->username) {
-        $allowed = true;
-    } elseif (!$U->isHidden($usr['username'])) {
-        if (isAllowed("calendarviewall")) {
-            $allowed = true;
-        } elseif (isAllowed("calendarviewgroup") && $UG->shareGroups($usr['username'], $UL->username)) {
-            $allowed = true;
-        }
+  $allowed = false;
+  if ($usr['username'] == $UL->username) {
+    $allowed = true;
+  } elseif (!$U->isHidden($usr['username'])) {
+    if (isAllowed("calendarviewall") || isAllowed("calendarviewgroup") && $UG->shareGroups($usr['username'], $UL->username)) {
+      $allowed = true;
     }
-    if ($allowed) {
-        $viewData['users'][] = array('username' => $usr['username'], 'lastfirst' => $U->getLastFirst($usr['username']));
-    }
+  }
+  if ($allowed) {
+    $viewData['users'][] = array( 'username' => $usr['username'], 'lastfirst' => $U->getLastFirst($usr['username']) );
+  }
 }
-
 $color = $H->getColor(2);
 $bgcolor = $H->getBgColor(2);
 $viewData['satStyle'] = ' style="color: #' . $color . '; background-color: #' . $bgcolor . ';"';
-
 $color = $H->getColor(3);
 $bgcolor = $H->getBgColor(3);
 $viewData['sunStyle'] = ' style="color: #' . $color . '; background-color: #' . $bgcolor . ';"';
@@ -241,7 +231,7 @@ $viewData['sunStyle'] = ' style="color: #' . $color . '; background-color: #' . 
 //
 // SHOW VIEW
 //
-require WEBSITE_ROOT . '/views/header.php';
-require WEBSITE_ROOT . '/views/menu.php';
-include WEBSITE_ROOT . '/views/' . $controller . '.php';
-require WEBSITE_ROOT . '/views/footer.php';
+require_once WEBSITE_ROOT . '/views/header.php';
+require_once WEBSITE_ROOT . '/views/menu.php';
+include_once WEBSITE_ROOT . '/views/' . $controller . '.php';
+require_once WEBSITE_ROOT . '/views/footer.php';
