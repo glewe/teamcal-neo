f5e90aaab1d05840e7775c9348f3b64de26e6a76
George Lewe <george@lewe.com>
2024-07-28 20:36:57 +0200

    Sonar fixe (calendarviewmonth view)

--- src/views/calendarviewmonth.php
+++ src/views/calendarviewmonth.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Caolendar View Month View
  *
@@ -11,19 +13,17 @@ if (!defined('VALID_ROOT')) { exit(''); }
  * @subpackage Views
  * @since 3.0.0
  */
-
-// echo "<script type=\"text/javascript\">alert(\"calendarviewuserrow.php: \");</script>";
 ?>
 
-  <!-- ====================================================================
-view.calendarviewmonth (<?= $viewData[ 'year' ] . $viewData[ 'month' ] ?>)
+<!-- ====================================================================
+view.calendarviewmonth (<?= $viewData['year'] . $viewData['month'] ?>)
 -->
-<?php if (!$viewData[ 'supportMobile' ]) {
-  $mobilecols = array( 'full' => $viewData[ 'dateInfo' ][ 'daysInMonth' ] );
+<?php if (!$viewData['supportMobile']) {
+  $mobilecols = array( 'full' => $viewData['dateInfo']['daysInMonth'] );
 } else {
-  switch ($viewData[ 'width' ]) {
+  switch ($viewData['width']) {
     case '1024plus':
-      $mobilecols = array( 'full' => $viewData[ 'dateInfo' ][ 'daysInMonth' ] );
+      $mobilecols = array( 'full' => $viewData['dateInfo']['daysInMonth'] );
       break;
 
     case '1024':
@@ -55,25 +55,28 @@ view.calendarviewmonth (<?= $viewData[ 'year' ] . $viewData[ 'month' ] ?>)
       break;
 
     default:
-      $mobilecols = array( 'full' => $viewData[ 'dateInfo' ][ 'daysInMonth' ] );
+      $mobilecols = array( 'full' => $viewData['dateInfo']['daysInMonth'] );
       break;
   }
 }
 
 foreach ($mobilecols as $key => $cols) {
-  $days = $viewData[ 'dateInfo' ][ 'daysInMonth' ];
+  $days = $viewData['dateInfo']['daysInMonth'];
   $tables = ceil($days / $cols);
   $script = '';
   for ($t = 0; $t < $tables; $t++) {
     $daystart = ($t * $cols) + 1;
     $daysleft = $days - ($cols * $t);
-    if ($daysleft >= $cols) $dayend = $daystart + ($cols - 1);
-    else $dayend = $days;
+    if ($daysleft >= $cols) {
+      $dayend = $daystart + ($cols - 1);
+    } else {
+      $dayend = $days;
+    }
     ?>
-    <div class="table<?= ($viewData[ 'supportMobile' ]) ? $key : ''; ?>">
+    <div class="table<?= ($viewData['supportMobile']) ? $key : ''; ?>">
       <table class="table table-bordered month">
 
-        <?php require("calendarviewmonthheader.php"); ?>
+        <?php require_once "calendarviewmonthheader.php"; ?>
 
         <!-- Rows 4ff: Users -->
         <?php
@@ -92,78 +95,88 @@ foreach ($mobilecols as $key => $cols) {
           // All-by-Group Display
           //
           $repeatHeaderCount = $C->read("repeatHeaderCount");
-          if ($repeatHeaderCount) $rowcount = 1;
+          if ($repeatHeaderCount) {
+            $rowcount = 1;
+          }
           //
           // Loop through all groups
           //
-          foreach ($viewData[ 'groups' ] as $grp) {
+          foreach ($viewData['groups'] as $grp) {
             $groupHeader = false;
             //
             // Loop through all users of this group
             //
-            foreach ($viewData[ 'users' ] as $usr) {
-              if (count($viewData[ 'groups' ]) == 1) {
+            foreach ($viewData['users'] as $usr) {
+              if (count($viewData['groups']) == 1) {
                 //
                 // Single group only, we will show the guests as well
                 //
                 if ($repeatHeaderCount && $rowcount > $repeatHeaderCount) {
-                  require("calendarviewmonthheader.php");
+                  require_once "calendarviewmonthheader.php";
                   $rowcount = 1;
                 }
                 if (!$groupHeader) { ?>
-                  <!-- Row: Group <?= $grp[ 'name' ] ?> -->
+                  <!-- Row: Group <?= $grp['name'] ?> -->
                   <tr>
-                    <th class="m-groupname" colspan="<?= $days + 1 ?>"><?= $grp[ 'description' ] . ' (' . $grp[ 'name' ] . ')' ?></th>
+                    <th id="th-<?= $grp['name'] ?>" class="m-groupname" colspan="<?= $days + 1 ?>"><?= $grp['description'] . ' (' . $grp['name'] . ')' ?></th>
                   </tr>
                   <?php $groupHeader = true;
                 }
                 //
                 // Skip if user is group manager and hideManagers is enabled
                 //
-                if ($C->read('hideManagers') and $UG->isGroupManagerOfGroup($usr[ 'username' ], $grp[ 'id' ])) {
+                if ($C->read('hideManagers') && $UG->isGroupManagerOfGroup($usr['username'], $grp['id'])) {
                   continue;
                 }
                 ?>
 
-                <!-- Row: User <?= $usr[ 'username' ] ?> -->
-                <?php require("calendarviewuserrow.php");
-                if ($repeatHeaderCount) $rowcount++;
+                <!-- Row: User <?= $usr['username'] ?> -->
+                <?php require_once "calendarviewuserrow.php";
+                if ($repeatHeaderCount) {
+                  $rowcount++;
+                }
               } else {
                 //
                 // Multiple groups, we will not show the guests
                 //
-                if ($UG->isMemberOrManagerOfGroup($usr[ 'username' ], $grp[ 'id' ])) {
-                  if ($repeatHeaderCount and $rowcount > $repeatHeaderCount) {
-                    require("calendarviewmonthheader.php");
+                if ($UG->isMemberOrManagerOfGroup($usr['username'], $grp['id'])) {
+                  if ($repeatHeaderCount && $rowcount > $repeatHeaderCount) {
+                    require_once "calendarviewmonthheader.php";
                     $rowcount = 1;
                   }
 
                   if (!$groupHeader) { ?>
-                    <!-- Row: Group <?= $grp[ 'name' ] ?> -->
+                    <!-- Row: Group <?= $grp['name'] ?> -->
                     <tr>
-                      <th class="m-groupname" colspan="<?= $days + 1 ?>"><?= $grp[ 'description' ] . ' (' . $grp[ 'name' ] . ')' ?></th>
+                      <th id="th-<?= $grp['name'] ?>" class="m-groupname" colspan="<?= $days + 1 ?>"><?= $grp['description'] . ' (' . $grp['name'] . ')' ?></th>
                     </tr>
                     <?php $groupHeader = true;
                   } ?>
 
-                  <!-- Row: User <?= $usr[ 'username' ] ?> -->
-                  <?php require("calendarviewuserrow.php");
-                  if ($repeatHeaderCount) $rowcount++;
+                  <!-- Row: User <?= $usr['username'] ?> -->
+                  <?php require_once "calendarviewuserrow.php";
+                  if ($repeatHeaderCount) {
+                    $rowcount++;
+                  }
                 }
               }
             }
           }
         } else {
           $repeatHeaderCount = $C->read("repeatHeaderCount");
-          if ($repeatHeaderCount) $rowcount = 1;
-          foreach ($viewData[ 'users' ] as $usr) {
-            if ($repeatHeaderCount and $rowcount > $repeatHeaderCount) {
-              require("calendarviewmonthheader.php");
+          if ($repeatHeaderCount) {
+            $rowcount = 1;
+          }
+          foreach ($viewData['users'] as $usr) {
+            if ($repeatHeaderCount && $rowcount > $repeatHeaderCount) {
+              require_once "calendarviewmonthheader.php";
               $rowcount = 1;
             } ?>
-            <!-- Row: User <?= $usr[ 'username' ] ?> -->
-            <?php require("calendarviewuserrow.php");
-            if ($repeatHeaderCount) $rowcount++;
+            <!-- Row: User <?= $usr['username'] ?> -->
+            <?php require_once "calendarviewuserrow.php";
+            if ($repeatHeaderCount) {
+              $rowcount++;
+            }
           }
         } // End if AllByGroup
         ?>
@@ -172,32 +185,34 @@ foreach ($mobilecols as $key => $cols) {
           <!-- Row: Summary Header -->
           <tr>
             <td class="m-label" colspan="<?= $dayend - $daystart + 2 ?>">
-              <span style="float: left;"><?= $LANG[ 'cal_summary' ] ?>&nbsp;<a class="btn btn-secondary btn-sm" data-bs-toggle="collapse" data-bs-target=".summary">...</a></span>
-              <span class="float-end text-normal"><?= $viewData[ 'businessDays' ] ?>&nbsp;<?= $LANG[ 'cal_businessDays' ] ?></span>
+              <span style="float: left;"><?= $LANG['cal_summary'] ?>&nbsp;<a class="btn btn-secondary btn-sm" data-bs-toggle="collapse" data-bs-target=".summary">...</a></span>
+              <span class="float-end text-normal"><?= $viewData['businessDays'] ?>&nbsp;<?= $LANG['cal_businessDays'] ?></span>
             </td>
           </tr>
 
           <!-- Row: Summary Absences -->
           <tr class="summary <?= (!$C->read('showSummary')) ? 'collapse' : 'in'; ?>">
-            <td class="m-name"><?= $LANG[ 'sum_absent' ] ?></td>
+            <td class="m-name"><?= $LANG['sum_absent'] ?></td>
             <?php for ($i = $daystart; $i <= $dayend; $i++) {
-              // $style = substr($viewData['dayStyles'][$i], 14);
-              $style = $viewData[ 'dayStyles' ][ $i ];
-              if (strlen($style)) $style = ' style="' . $style . '"';
+              $style = $viewData['dayStyles'][$i];
+              if (strlen($style)) {
+                $style = ' style="' . $style . '"';
+              }
               ?>
-              <td class="m-day m-summary text-center text-danger" <?= $style ?>><?= $dayAbsCount[ $i ] ?></td>
+              <td class="m-day m-summary text-center text-danger" <?= $style ?>><?= $dayAbsCount[$i] ?></td>
             <?php } ?>
           </tr>
 
           <!-- Row: Summary Presences -->
           <tr class="summary <?= (!$C->read('showSummary')) ? 'collapse' : 'in'; ?>">
-            <td class="m-name"><?= $LANG[ 'sum_present' ] ?></td>
+            <td class="m-name"><?= $LANG['sum_present'] ?></td>
             <?php for ($i = $daystart; $i <= $dayend; $i++) {
-              // $style = substr($viewData['dayStyles'][$i], 14);
-              $style = $viewData[ 'dayStyles' ][ $i ];
-              if (strlen($style)) $style = ' style="' . $style . '"';
+              $style = $viewData['dayStyles'][$i];
+              if (strlen($style)) {
+                $style = ' style="' . $style . '"';
+              }
               ?>
-              <td class="m-day m-summary text-center text-success" <?= $style ?>><?= $dayPresCount[ $i ] ?></td>
+              <td class="m-day m-summary text-center text-success" <?= $style ?>><?= $dayPresCount[$i] ?></td>
             <?php } ?>
           </tr>
         <?php } ?>
