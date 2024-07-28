f075dd69d6535fd1dfd0d7b02778574644731323
George Lewe <george@lewe.com>
2024-07-28 20:29:44 +0200

    Sonar fixe (calendaroptions view)

--- src/views/calendaroptions.php
+++ src/views/calendaroptions.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Calendar Options View
  *
@@ -20,41 +22,42 @@ view.calendaroptions
 
   <div class="col-lg-12">
     <?php
-    if ($showAlert && $C->read("showAlerts") != "none") {
-      if (
-        $C->read("showAlerts") == "all" or
-        $C->read("showAlerts") == "warnings" && ($alertData[ 'type' ] == "warning" or $alertData[ 'type' ] == "danger")
-      ) {
-        echo createAlertBox($alertData);
-      }
-    } ?>
-    <?php $tabindex = 1;
+    if (
+      ($showAlert && $C->read("showAlerts") != "none") &&
+      ($C->read("showAlerts") == "all" || $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" || $alertData['type'] == "danger"))
+    ) {
+      echo createAlertBox($alertData);
+    }
+    $tabindex = 1;
     $colsleft = 8;
-    $colsright = 4; ?>
+    $colsright = 4;
+    ?>
 
     <form class="form-control-horizontal" action="index.php?action=<?= $controller ?>" method="post" target="_self" accept-charset="utf-8">
 
       <div class="card">
         <?php
         $pageHelp = '';
-        if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF[ 'controllers' ][ $controller ]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+        if ($C->read('pageHelp')) {
+          $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+        }
         ?>
-        <div class="card-header text-white bg-<?= $CONF[ 'controllers' ][ $controller ]->panelColor ?>"><i class="<?= $CONF[ 'controllers' ][ $controller ]->faIcon ?> fa-lg me-3"></i><?= $LANG[ 'calopt_title' ] ?><?= $pageHelp ?></div>
+        <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['calopt_title'] ?><?= $pageHelp ?></div>
         <div class="card-body">
 
           <div class="card">
             <div class="card-body">
-              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_caloptApply"><?= $LANG[ 'btn_apply' ] ?></button>
+              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_caloptApply"><?= $LANG['btn_apply'] ?></button>
             </div>
           </div>
           <div style="height:20px;"></div>
 
           <ul class="nav nav-tabs" role="tablist">
-            <li class="nav-item"><a class="nav-link active" id="display-tab" href="#display" data-bs-toggle="tab" role="tab" aria-controls="display" aria-selected="true"><?= $LANG[ 'calopt_tab_display' ] ?></a></li>
-            <li class="nav-item"><a class="nav-link" id="filter-tab" href="#filter" data-bs-toggle="tab" role="tab" aria-controls="filter" aria-selected="false"><?= $LANG[ 'calopt_tab_filter' ] ?></a></li>
-            <li class="nav-item"><a class="nav-link" id="options-tab" href="#options" data-bs-toggle="tab" role="tab" aria-controls="options" aria-selected="false"><?= $LANG[ 'calopt_tab_options' ] ?></a></li>
-            <li class="nav-item"><a class="nav-link" id="stats-tab" href="#stats" data-bs-toggle="tab" role="tab" aria-controls="stats" aria-selected="false"><?= $LANG[ 'calopt_tab_stats' ] ?></a></li>
-            <li class="nav-item"><a class="nav-link" id="summary-tab" href="#summary" data-bs-toggle="tab" role="tab" aria-controls="summary" aria-selected="false"><?= $LANG[ 'calopt_tab_summary' ] ?></a></li>
+            <li class="nav-item"><a class="nav-link active" id="display-tab" href="#display" data-bs-toggle="tab" role="tab" aria-controls="display" aria-selected="true"><?= $LANG['calopt_tab_display'] ?></a></li>
+            <li class="nav-item"><a class="nav-link" id="filter-tab" href="#filter" data-bs-toggle="tab" role="tab" aria-controls="filter" aria-selected="false"><?= $LANG['calopt_tab_filter'] ?></a></li>
+            <li class="nav-item"><a class="nav-link" id="options-tab" href="#options" data-bs-toggle="tab" role="tab" aria-controls="options" aria-selected="false"><?= $LANG['calopt_tab_options'] ?></a></li>
+            <li class="nav-item"><a class="nav-link" id="stats-tab" href="#stats" data-bs-toggle="tab" role="tab" aria-controls="stats" aria-selected="false"><?= $LANG['calopt_tab_stats'] ?></a></li>
+            <li class="nav-item"><a class="nav-link" id="summary-tab" href="#summary" data-bs-toggle="tab" role="tab" aria-controls="summary" aria-selected="false"><?= $LANG['calopt_tab_summary'] ?></a></li>
           </ul>
 
           <div id="myTabContent" class="tab-content">
@@ -63,7 +66,7 @@ view.calendaroptions
             <div class="tab-pane fade show active" id="display" role="tabpanel" aria-labelledby="display-tab">
               <div class="card">
                 <div class="card-body">
-                  <?php foreach ($caloptData[ 'display' ] as $formObject) {
+                  <?php foreach ($caloptData['display'] as $formObject) {
                     echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
                   } ?>
                 </div>
@@ -74,7 +77,7 @@ view.calendaroptions
             <div class="tab-pane fade" id="filter" role="tabpanel" aria-labelledby="filter-tab">
               <div class="card">
                 <div class="card-body">
-                  <?php foreach ($caloptData[ 'filter' ] as $formObject) {
+                  <?php foreach ($caloptData['filter'] as $formObject) {
                     echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
                   } ?>
                 </div>
@@ -85,7 +88,7 @@ view.calendaroptions
             <div class="tab-pane fade" id="options" role="tabpanel" aria-labelledby="options-tab">
               <div class="card">
                 <div class="card-body">
-                  <?php foreach ($caloptData[ 'options' ] as $formObject) {
+                  <?php foreach ($caloptData['options'] as $formObject) {
                     echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
                   } ?>
                 </div>
@@ -96,7 +99,7 @@ view.calendaroptions
             <div class="tab-pane fade" id="summary" role="tabpanel" aria-labelledby="summary-tab">
               <div class="card">
                 <div class="card-body">
-                  <?php foreach ($caloptData[ 'summary' ] as $formObject) {
+                  <?php foreach ($caloptData['summary'] as $formObject) {
                     echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
                   } ?>
                 </div>
@@ -107,7 +110,7 @@ view.calendaroptions
             <div class="tab-pane fade" id="stats" role="tabpanel" aria-labelledby="stats-tab">
               <div class="card">
                 <div class="card-body">
-                  <?php foreach ($caloptData[ 'stats' ] as $formObject) {
+                  <?php foreach ($caloptData['stats'] as $formObject) {
                     echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
                   } ?>
                 </div>
@@ -119,7 +122,7 @@ view.calendaroptions
           <div style="height:20px;"></div>
           <div class="card">
             <div class="card-body">
-              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_caloptApply"><?= $LANG[ 'btn_apply' ] ?></button>
+              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_caloptApply"><?= $LANG['btn_apply'] ?></button>
             </div>
           </div>
 
