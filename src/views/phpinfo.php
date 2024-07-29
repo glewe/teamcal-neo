3548e8024cd08f484a271e36c9246f1e369d0c45
George Lewe <george@lewe.com>
2024-07-29 19:23:50 +0200

    Sonar fixe (phpinfo view)

--- src/views/phpinfo.php
+++ src/views/phpinfo.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * PHP Info View
  *
@@ -17,18 +19,18 @@ if (!defined('VALID_ROOT')) { exit(''); }
 view.phpinfo
 -->
 <div class="container content">
-
-    <div class="col-lg-12">
-        <div class="card">
-            <?php
-            $pageHelp = '';
-            if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-            ?>
-            <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['phpinfo_title'] . $pageHelp ?></div>
-            <div class="card-body">
-                <?= $viewData['phpInfo'] ?>
-            </div>
-        </div>
+  <div class="col-lg-12">
+    <div class="card">
+      <?php
+      $pageHelp = '';
+      if ($C->read('pageHelp')) {
+        $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+      }
+      ?>
+      <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['phpinfo_title'] . $pageHelp ?></div>
+      <div class="card-body">
+        <?= $viewData['phpInfo'] ?>
+      </div>
     </div>
-
+  </div>
 </div>
