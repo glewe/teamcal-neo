931658e7c71de714a34c60190f7b07ceb647e2d1
George Lewe <george@lewe.com>
2024-07-29 19:08:31 +0200

    Sonar fixe (home view)

--- src/views/home.php
+++ src/views/home.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Home View
  *
@@ -18,48 +20,47 @@ view.home
 -->
 <div class="container content">
 
-    <div class="col-lg-12">
+  <div class="col-lg-12">
 
-        <?php
-        if ($showAlert && $C->read("showAlerts") != "none") {
-            if (
-                $C->read("showAlerts") == "all" or
-                $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" or $alertData['type'] == "danger")
-            ) {
-                echo createAlertBox($alertData);
-            }
-        } ?>
+    <?php
+    if (
+      ($showAlert && $C->read("showAlerts") != "none") &&
+      ($C->read("showAlerts") == "all" || $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" || $alertData['type'] == "danger"))
+    ) {
+      echo createAlertBox($alertData);
+    }
+    ?>
 
-        <div class="card text-<?= $CONF['controllers'][$controller]->panelColor ?>">
-            <?php
-            $pageHelp = '';
-            if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-            ?>
-            <div class="card-header"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['home_title'] . $pageHelp ?></div>
-            <div class="card-body">
-                <?php echo $C->read("welcomeText"); ?>
-            </div>
-        </div>
+    <div class="card text-<?= $CONF['controllers'][$controller]->panelColor ?>">
+      <?php
+      $pageHelp = '';
+      if ($C->read('pageHelp')) {
+        $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+      }
+      ?>
+      <div class="card-header"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['home_title'] . $pageHelp ?></div>
+      <div class="card-body">
+        <?php echo $C->read("welcomeText"); ?>
+      </div>
     </div>
+  </div>
 
 </div>
 
 <script>
-    // Check if a new cache is available on page load.
-    window.addEventListener('load', function(e) {
-
-        window.applicationCache.addEventListener('updateready', function(e) {
-            if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
-                // Browser downloaded a new app cache.
-                // Swap it in and reload the page to get the new hotness.
-                window.applicationCache.swapCache();
-                if (confirm('A new version of this site is available. Load it?')) {
-                    window.location.reload();
-                }
-            } else {
-                // Manifest didn't changed. Nothing new to server.
-            }
-        }, false);
-
+  // Check if a new cache is available on page load.
+  window.addEventListener('load', function (e) {
+    window.applicationCache.addEventListener('updateready', function (e) {
+      if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+        // Browser downloaded a new app cache.
+        // Swap it in and reload the page to get the new hotness.
+        window.applicationCache.swapCache();
+        if (confirm('A new version of this site is available. Load it?')) {
+          window.location.reload();
+        }
+      } else {
+        // Manifest didn't changed. Nothing new to server.
+      }
     }, false);
+  }, false);
 </script>
