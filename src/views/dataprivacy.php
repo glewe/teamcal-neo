2fa8bc5edbd3fa4ca6396488b4c2d65c981dc33e
George Lewe <george@lewe.com>
2024-07-29 08:51:51 +0200

    Sonar fixe (dataprivacy view)

--- src/views/dataprivacy.php
+++ src/views/dataprivacy.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Data Privacy View
  *
@@ -18,17 +20,19 @@ view.dataprivacy
 -->
 <div class="container content">
 
-    <div class="col-lg-12">
-        <div class="card">
-            <?php
-            $pageHelp = '';
-            if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-            ?>
-            <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['gdpr_title'] . $pageHelp ?></div>
-            <div class="card-body">
-                <div class="col-lg-12 gdpr"><?= $viewData['gdpr_text'] ?></div>
-            </div>
-        </div>
+  <div class="col-lg-12">
+    <div class="card">
+      <?php
+      $pageHelp = '';
+      if ($C->read('pageHelp')) {
+        $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+      }
+      ?>
+      <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['gdpr_title'] . $pageHelp ?></div>
+      <div class="card-body">
+        <div class="col-lg-12 gdpr"><?= $viewData['gdpr_text'] ?></div>
+      </div>
     </div>
+  </div>
 
 </div>
