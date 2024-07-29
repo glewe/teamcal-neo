fb1a4d7b40568345809efac71c00bb505de6147e
George Lewe <george@lewe.com>
2024-07-29 19:11:31 +0200

    Sonar fixe (login view)

--- src/views/login.php
+++ src/views/login.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Login View
  *
@@ -17,67 +19,66 @@ if (!defined('VALID_ROOT')) { exit(''); }
 view.login
 -->
 <div class="container content">
-    <div class="row">
+  <div class="row">
 
-        <div class="col-lg-3"></div>
+    <div class="col-lg-3"></div>
 
-        <div class="col-lg-6">
-            <?php
-            if ($showAlert && $C->read("showAlerts") != "none") {
-                if (
-                    $C->read("showAlerts") == "all" or
-                    $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" or $alertData['type'] == "danger")
-                ) {
-                    echo createAlertBox($alertData);
-                }
-            } ?>
-            <div class="card">
-                <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['login_login'] ?></div>
-                <div class="card-body">
-                    <div class="col-lg-12">
-                        <?php $tabindex = 1;
-                        $colsleft = 5;
-                        $colsright = 7;
-                        $paddingBottom = "12px"; ?>
-                        <form id="login" action="index.php?action=<?= $controller ?>" method="post" target="_self" name="loginform" accept-charset="utf-8">
-                            <fieldset>
-                                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
-                                    <label class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_username'] ?></label>
-                                    <div class="col-lg-<?= $colsright ?>">
-                                        <input id="inputUsername" class="form-control" autofocus="autofocus" tabindex="1" name="uname" type="text" value="<?= (isset($uname)) ? $uname : ""; ?>">
-                                    </div>
-                                </div>
-                                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
-                                    <label class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_password'] ?></label>
-                                    <div class="col-lg-<?= $colsright ?>">
-                                        <input class="form-control" name="pword" tabindex="2" type="password" autocomplete="off">
-                                    </div>
-                                </div>
-                                <hr>
-                                <?= $LANG['login_authcode_comment'] ?>
-                                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
-                                    <label class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_authcode'] ?></label>
-                                    <div class="col-lg-<?= $colsright ?>">
-                                        <input id="totp" class="form-control" name="totp" type="text" minlength="6" maxlength="6" pattern="^[0-9]{1,6}$">
-                                    </div>
-                                </div>
-                                <hr>
-                                <div class="form-group row">
-                                    <label class="col-lg-<?= $colsleft ?> control-label"></label>
-                                    <div class="col-lg-<?= $colsright ?>">
-                                        <input id="inputSubmit" name="btn_login" type="text" value="true" style="visibility: hidden; display: none">
-                                        <button type="submit" class="btn btn-primary" tabindex="3" name="submit"><?= $LANG['btn_login'] ?></button>
-                                        <a href="index.php?action=passwordrequest" class="btn btn-secondary float-end" tabindex="4"><?= $LANG['btn_reset_password'] ?></a>
-                                    </div>
-                                </div>
-                            </fieldset>
-                        </form>
-                    </div>
+    <div class="col-lg-6">
+      <?php
+      if (
+        ($showAlert && $C->read("showAlerts") != "none") &&
+        ($C->read("showAlerts") == "all" || $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" || $alertData['type'] == "danger"))
+      ) {
+        echo createAlertBox($alertData);
+      }
+      $tabindex = 1;
+      $colsleft = 5;
+      $colsright = 7;
+      $paddingBottom = "12px";
+      ?>
+      <div class="card">
+        <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['login_login'] ?></div>
+        <div class="card-body">
+          <div class="col-lg-12">
+            <form id="login" action="index.php?action=<?= $controller ?>" method="post" target="_self" name="loginform" accept-charset="utf-8">
+              <fieldset>
+                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
+                  <label for="inputUsername" class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_username'] ?></label>
+                  <div class="col-lg-<?= $colsright ?>">
+                    <input id="inputUsername" class="form-control" autofocus="autofocus" tabindex="1" name="uname" type="text" value="<?= (isset($uname)) ? $uname : ""; ?>">
+                  </div>
                 </div>
-            </div>
+                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
+                  <label for="pword" class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_password'] ?></label>
+                  <div class="col-lg-<?= $colsright ?>">
+                    <input class="form-control" id="pword" name="pword" tabindex="2" type="password" autocomplete="off">
+                  </div>
+                </div>
+                <hr>
+                <?= $LANG['login_authcode_comment'] ?>
+                <div class="form-group row" style="padding-bottom: <?= $paddingBottom ?>;">
+                  <label for="totp" class="col-lg-<?= $colsleft ?> control-label"><?= $LANG['login_authcode'] ?></label>
+                  <div class="col-lg-<?= $colsright ?>">
+                    <input id="totp" class="form-control" name="totp" type="text" minlength="6" maxlength="6" pattern="^[0-9]{1,6}$">
+                  </div>
+                </div>
+                <hr>
+                <div class="form-group row">
+                  <label for="inputSubmit" class="col-lg-<?= $colsleft ?> control-label"></label>
+                  <div class="col-lg-<?= $colsright ?>">
+                    <input id="inputSubmit" name="btn_login" type="text" value="true" style="visibility: hidden; display: none">
+                    <button type="submit" class="btn btn-primary" tabindex="3" name="submit"><?= $LANG['btn_login'] ?></button>
+                    <a href="index.php?action=passwordrequest" class="btn btn-secondary float-end" tabindex="4"><?= $LANG['btn_reset_password'] ?></a>
+                  </div>
+                </div>
+              </fieldset>
+            </form>
+          </div>
         </div>
-
-        <div class="col-lg-3"></div>
-
+      </div>
     </div>
+
+    <div class="col-lg-3"></div>
+
+  </div>
 </div>
