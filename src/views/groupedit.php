73c39ece346ff73eb8021b72f1ced5bcf10fc7c2
George Lewe <george@lewe.com>
2024-07-29 09:16:58 +0200

    Sonar fixe (groupedit view)

--- src/views/groupedit.php
+++ src/views/groupedit.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Group Edit View
  *
@@ -18,87 +20,88 @@ view.groupedit
 -->
 <div class="container content">
 
-    <div class="col-lg-12">
+  <div class="col-lg-12">
+    <?php
+    if (
+      ($showAlert && $C->read("showAlerts") != "none") &&
+      ($C->read("showAlerts") == "all" || $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" || $alertData['type'] == "danger"))
+    ) {
+      echo createAlertBox($alertData);
+    }
+    $tabindex = 1;
+    $colsleft = 8;
+    $colsright = 4;
+    ?>
+
+    <form class="form-control-horizontal" enctype="multipart/form-data" action="index.php?action=<?= $controller ?>&amp;id=<?= $viewData['id'] ?>" method="post" target="_self" accept-charset="utf-8">
+
+      <div class="card">
         <?php
-        if ($showAlert && $C->read("showAlerts") != "none") {
-            if (
-                $C->read("showAlerts") == "all" or
-                $C->read("showAlerts") == "warnings" && ($alertData['type'] == "warning" or $alertData['type'] == "danger")
-            ) {
-                echo createAlertBox($alertData);
-            }
-        } ?>
-        <?php $tabindex = 1;
-        $colsleft = 8;
-        $colsright = 4; ?>
+        $pageHelp = '';
+        if ($C->read('pageHelp')) {
+          $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+        }
+        ?>
+        <div class="card-header panel-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['group_edit_title'] . $viewData['name'] . $pageHelp ?></div>
+        <div class="card-body">
 
-        <form class="form-control-horizontal" enctype="multipart/form-data" action="index.php?action=<?= $controller ?>&amp;id=<?= $viewData['id'] ?>" method="post" target="_self" accept-charset="utf-8">
+          <div class="card">
+            <div class="card-body">
+              <input name="hidden_id" type="hidden" value="<?= $viewData['id'] ?>">
+              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_groupUpdate"><?= $LANG['btn_update'] ?></button>
+              <a href="index.php?action=groups" class="btn btn-secondary float-end" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_group_list'] ?></a>
+            </div>
+          </div>
+          <div style="height:20px;"></div>
 
-            <div class="card">
-                <?php
-                $pageHelp = '';
-                if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-                ?>
-                <div class="card-header panel-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['group_edit_title'] . $viewData['name'] . $pageHelp ?></div>
+          <ul class="nav nav-tabs" role="tablist">
+            <li class="nav-item"><a class="nav-link active" id="tab_settings-tab" href="#tab_settings" data-bs-toggle="tab" role="tab" aria-controls="tab_settings" aria-selected="true"><?= $LANG['group_tab_settings'] ?></a></li>
+            <li class="nav-item"><a class="nav-link" id="tab_members-tab" href="#tab_members" data-bs-toggle="tab" role="tab" aria-controls="tab_members" aria-selected="false"><?= $LANG['group_tab_members'] ?></a></li>
+          </ul>
+
+          <div id="myTabContent" class="tab-content">
+
+            <!-- Group Settings -->
+            <div class="tab-pane fade show active" id="tab_settings" role="tabpanel" aria-labelledby="tab_settings-tab">
+              <div class="card">
                 <div class="card-body">
-
-                    <div class="card">
-                        <div class="card-body">
-                            <input name="hidden_id" type="hidden" value="<?= $viewData['id'] ?>">
-                            <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_groupUpdate"><?= $LANG['btn_update'] ?></button>
-                            <a href="index.php?action=groups" class="btn btn-secondary float-end" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_group_list'] ?></a>
-                        </div>
-                    </div>
-                    <div style="height:20px;"></div>
-
-                    <ul class="nav nav-tabs" role="tablist">
-                        <li class="nav-item"><a class="nav-link active" id="tab_settings-tab" href="#tab_settings" data-bs-toggle="tab" role="tab" aria-controls="tab_settings" aria-selected="true"><?= $LANG['group_tab_settings'] ?></a></li>
-                        <li class="nav-item"><a class="nav-link" id="tab_members-tab" href="#tab_members" data-bs-toggle="tab" role="tab" aria-controls="tab_members" aria-selected="false"><?= $LANG['group_tab_members'] ?></a></li>
-                    </ul>
-
-                    <div id="myTabContent" class="tab-content">
-
-                        <!-- Group Settings -->
-                        <div class="tab-pane fade show active" id="tab_settings" role="tabpanel" aria-labelledby="tab_settings-tab">
-                            <div class="card">
-                                <div class="card-body">
-                                    <?php foreach ($viewData['group'] as $formObject) {
-                                        echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
-                                    } ?>
-                                </div>
-                            </div>
-                        </div>
-
-                        <!-- Group Members -->
-                        <div class="tab-pane fade" id="tab_members" role="tabpanel" aria-labelledby="tab_members-tab">
-                            <div class="card">
-                                <div class="card-body">
-                                    <?php foreach ($viewData['members'] as $formObject) {
-                                        echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
-                                    } ?>
-                                    <?php foreach ($viewData['managers'] as $formObject) {
-                                        echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
-                                    } ?>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
-
-                    <div style="height:20px;"></div>
-                    <div class="card">
-                        <div class="card-body">
-                            <input name="hidden_id" type="hidden" value="<?= $viewData['id'] ?>">
-                            <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_groupUpdate"><?= $LANG['btn_update'] ?></button>
-                            <a href="index.php?action=groups" class="btn btn-secondary float-end" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_group_list'] ?></a>
-                        </div>
-                    </div>
-
+                  <?php foreach ($viewData['group'] as $formObject) {
+                    echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
+                  } ?>
                 </div>
+              </div>
             </div>
 
-        </form>
+            <!-- Group Members -->
+            <div class="tab-pane fade" id="tab_members" role="tabpanel" aria-labelledby="tab_members-tab">
+              <div class="card">
+                <div class="card-body">
+                  <?php foreach ($viewData['members'] as $formObject) {
+                    echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
+                  } ?>
+                  <?php foreach ($viewData['managers'] as $formObject) {
+                    echo createFormGroup($formObject, $colsleft, $colsright, $tabindex++);
+                  } ?>
+                </div>
+              </div>
+            </div>
 
-    </div>
+          </div>
+
+          <div style="height:20px;"></div>
+          <div class="card">
+            <div class="card-body">
+              <input name="hidden_id" type="hidden" value="<?= $viewData['id'] ?>">
+              <button type="submit" class="btn btn-primary" tabindex="<?= $tabindex++ ?>" name="btn_groupUpdate"><?= $LANG['btn_update'] ?></button>
+              <a href="index.php?action=groups" class="btn btn-secondary float-end" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_group_list'] ?></a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+
+    </form>
+
+  </div>
 
 </div>
