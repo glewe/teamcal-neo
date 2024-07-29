010e05a8643ebb6eb73ad69a198b8bbd96f74114
George Lewe <george@lewe.com>
2024-07-29 19:14:30 +0200

    Sonar fixe (maintenance view)

--- src/views/maintenance.php
+++ src/views/maintenance.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Maintenance View
  *
@@ -13,29 +15,31 @@ if (!defined('VALID_ROOT')) { exit(''); }
  */
 ?>
 
-<!-- ==================================================================== 
+<!-- ====================================================================
 view.maintenance
 -->
 <div class="container content">
 
-    <div class="col-lg-12">
-        <div class="card">
-            <?php
-            $pageHelp = '';
-            if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-            ?>
-            <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['mtce_title'] . $pageHelp ?></div>
-            <div class="card-body">
-                <div class="row">
-                    <div class="col-lg-3"><img src="images/maintenance.gif" alt="" class="img_floatleft">
-                    </div>
-                    <div class="col-lg-9">
-                        <h4><?= $LANG['mtce_title'] ?></h4>
-                        <p><?= $LANG['mtce_text'] ?></p>
-                    </div>
-                </div>
-            </div>
+  <div class="col-lg-12">
+    <div class="card">
+      <?php
+      $pageHelp = '';
+      if ($C->read('pageHelp')) {
+        $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+      }
+      ?>
+      <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['mtce_title'] . $pageHelp ?></div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-3"><img src="images/maintenance.gif" alt="" class="img_floatleft">
+          </div>
+          <div class="col-lg-9">
+            <h4><?= $LANG['mtce_title'] ?></h4>
+            <p><?= $LANG['mtce_text'] ?></p>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 
 </div>
