8318104b43c11a0e0833e2bad62c0dc87b850386
George Lewe <george@lewe.com>
2024-07-29 19:09:04 +0200

    Sonar fixe (imprint view)

--- src/views/imprint.php
+++ src/views/imprint.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Imprint View
  *
@@ -13,27 +15,29 @@ if (!defined('VALID_ROOT')) { exit(''); }
  */
 ?>
 
-<!-- ==================================================================== 
+<!-- ====================================================================
 view.imprint
 -->
 <div class="container content">
 
-    <div class="col-lg-12">
-        <div class="card">
-            <?php
-            $pageHelp = '';
-            if ($C->read('pageHelp')) $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
-            ?>
-            <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['mnu_help_imprint'] . $pageHelp ?></div>
-            <div class="card-body">
-                <div class="col-lg-12">
-                    <?php foreach ($LANG['imprint'] as $imprint) { ?>
-                        <h4 style="margin-top: 40px"><?= $imprint['title'] ?></h4>
-                        <?= $imprint['text'] ?>
-                    <?php } ?>
-                </div>
-            </div>
+  <div class="col-lg-12">
+    <div class="card">
+      <?php
+      $pageHelp = '';
+      if ($C->read('pageHelp')) {
+        $pageHelp = '<a href="' . $CONF['controllers'][$controller]->docurl . '" target="_blank" class="float-end" style="color:inherit;"><i class="fas fa-question-circle fa-lg"></i></a>';
+      }
+      ?>
+      <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['mnu_help_imprint'] . $pageHelp ?></div>
+      <div class="card-body">
+        <div class="col-lg-12">
+          <?php foreach ($LANG['imprint'] as $imprint) { ?>
+            <h4 style="margin-top: 40px"><?= $imprint['title'] ?></h4>
+            <?= $imprint['text'] ?>
+          <?php } ?>
         </div>
+      </div>
     </div>
+  </div>
 
 </div>
