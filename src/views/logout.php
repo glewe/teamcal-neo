78d6c4bc8cbe5596a72485769dd3609f4279fcfb
George Lewe <george@lewe.com>
2024-07-29 19:14:02 +0200

    Sonar fixe (logout view)

--- src/views/logout.php
+++ src/views/logout.php
@@ -1,5 +1,7 @@
 <?php
-if (!defined('VALID_ROOT')) { exit(''); }
+if (!defined('VALID_ROOT')) {
+  exit('');
+}
 /**
  * Logout View
  *
@@ -17,29 +19,29 @@ if (!defined('VALID_ROOT')) { exit(''); }
 view.logout
 -->
 <script>
-    document.cookie = '<?= $viewData['cookie_name'] ?>=; expires=Thu, 01 Jan 1970 00:00:01 UTC; path=/';
+  document.cookie = '<?= $viewData['cookie_name'] ?>=; expires=Thu, 01 Jan 1970 00:00:01 UTC; path=/';
 </script>
 <div class="container content">
-    <div class="row">
+  <div class="row">
 
-        <div class="col-lg-3"></div>
+    <div class="col-lg-3"></div>
 
-        <div class="col-lg-6">
-            <div class="card">
-                <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['logout_title'] ?></div>
-                <div class="card-body">
-                    <div class="col-lg-12">
-                        <p><?= $LANG['logout_comment'] ?></p>
-                        <div class="divider">
-                            <hr>
-                        </div>
-                        <a href="index.php?action=login" class="btn btn-primary" tabindex="1"><?= $LANG['btn_login'] ?></a>
-                    </div>
-                </div>
+    <div class="col-lg-6">
+      <div class="card">
+        <div class="card-header bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['logout_title'] ?></div>
+        <div class="card-body">
+          <div class="col-lg-12">
+            <p><?= $LANG['logout_comment'] ?></p>
+            <div class="divider">
+              <hr>
             </div>
+            <a href="index.php?action=login" class="btn btn-primary"><?= $LANG['btn_login'] ?></a>
+          </div>
         </div>
-
-        <div class="col-lg-3"></div>
-
+      </div>
     </div>
+
+    <div class="col-lg-3"></div>
+
+  </div>
 </div>
