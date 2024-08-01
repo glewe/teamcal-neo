e5300eec8855a8b0e9ffb9a025936737d4f9a0e8
George Lewe <george@lewe.com>
2024-08-01 15:07:04 +0200

    Fixed aria label of modal dialogs

--- src/classes/Log.class.php
+++ src/classes/Log.class.php
@@ -24,6 +24,7 @@ class Log {
   public $event = '';
   private $db = '';
   private $table = '';
+  private $C = '';
 
   // ---------------------------------------------------------------------
   /**
@@ -31,6 +32,7 @@ class Log {
    */
   public function __construct() {
     global $C, $CONF, $DB;
+    $this->C = $C;
     $this->db = $DB->db;
     $this->table = $CONF['db_table_log'];
   }
@@ -100,15 +102,15 @@ class Log {
    * @return boolean Query result
    */
   public function logEvent($type, $user, $event, $object = '') {
-    if (!strlen($C->read("logLanguage"))) {
+    if (!strlen($this->C->read("logLanguage"))) {
       $loglang = 'english';
     }
     else {
-      $loglang = $C->read("logLanguage");
+      $loglang = $this->C->read("logLanguage");
     }
     require_once WEBSITE_ROOT . "/languages/" . $loglang . ".log.php";
     $myEvent = $LANG[$event] . $object;
-    if ($C->read($type)) {
+    if ($this->C->read($type)) {
       $ts = date("YmdHis");
       $query = $this->db->prepare('INSERT INTO ' . $this->table . ' (type, timestamp, user, event) VALUES (:val1, :val2, :val3, :val4)');
       $query->bindParam('val1', $type);
