32d4076bb52572c4258ac1d9da2c67d62bf57354
George Lewe <george@lewe.com>
2024-09-06 17:30:56 +0200

    sample.sql update

--- src/sql/sample.sql
+++ src/sql/sample.sql
@@ -1,9 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Sep 06, 2024 at 03:37 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
