cc70b838df436e0db7dd15cc796d4af497be6189
George Lewe <george@lewe.com>
2024-09-16 16:58:37 +0200

    Improved installation database check

--- src/helpers/ajax_checkdb.php
+++ src/helpers/ajax_checkdb.php
@@ -22,20 +22,29 @@ if (strlen($_REQUEST['server']) && strlen($_REQUEST['db']) && strlen($_REQUEST['
     $prefix = '';
   }
   try {
+    // Connect to the database
     $pdo = new PDO('mysql:host=' . $_REQUEST['server'] . ';dbname=' . $_REQUEST['db'] . ';charset=utf8', $_REQUEST['user'], $_REQUEST['pass']);
-    $query = $pdo->prepare('SELECT * FROM ' . $prefix . 'users;');
-    $result = $query->execute();
-    if ($result && $query->rowCount()) {
-      $msg = "<div class='alert alert-success'><h5>Database Connection Test</h5><p>Connect to the mySQL server and database was successful.</p></div>";
-      $msg .= "<div class='alert alert-success'><h5>Table Test</h5><p>Tables with the given prefix exist.</p></div>";
+    // Query to check for tables in the database
+    $query = $pdo->prepare("SELECT COUNT(*) as table_count FROM information_schema.tables WHERE table_schema = :db");
+    $query->execute([ 'db' => $_REQUEST['db'] ]);
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+    if ($result['table_count'] === 0) {
+      $msg = "<div class='alert alert-success'><h5>Database Check</h5><p>The database exists and is empty.</p></div>";
     } else {
-      $msg = "<div class='alert alert-success'><h5>Database Connection Test</h5><p>Connect to the mySQL server and database was successful.</p></div>";
-      $msg .= "<div class='alert alert-warning'><h5>Table Test</h5><p>Tables with the given prefix do not exist.</p></div>";
+      $msg = "<div class='alert alert-warning'><h5>Database Check</h5><p>The database exists but is not empty.</p></div>";
+      $query2 = $pdo->prepare("SELECT COUNT(*) as table_count FROM information_schema.tables WHERE table_schema = '" . $_REQUEST['db'] . "' AND table_name LIKE '" . $prefix . "%'");
+      $query2->execute();
+      $result2 = $query2->fetch(PDO::FETCH_ASSOC);
+      if ($result2['table_count'] > 0) {
+        $msg .= "<div class='alert alert-warning'><h5>Table Check</h5><p>Tables with the given prefix exist (" . $result2['table_count'] . ").</p></div>";
+      } else {
+        $msg .= "<div class='alert alert-success'><h5>Table Check</h5><p>Tables with the given prefix do not exist (" . $result2['table_count'] . ").</p></div>";
+      }
     }
   } catch (PDOException $e) {
-    $msg = "<div class='alert alert-danger'><h5>Database Connection Test</h5><p>Connect to mySQL server and/or database failed.</p></div>";
+    $msg = "<div class='alert alert-danger'><h5>Database Check</h5><p>Failed to connect to the database: " . $e->getMessage() . "</p></div>";
   }
 } else {
-  $msg = "<div class='alert alert-danger'><h5>Database Connection Test</h5><p>You need to provide at least the database server, name and user.</p></div>";
+  $msg = "<div class='alert alert-danger'><h5>Database Check</h5><p>You need to provide at least the database server, name and user.</p></div>";
 }
 echo $msg;
