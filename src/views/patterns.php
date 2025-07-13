3a237be0ef06aaee40bf54dd00bc10380f615d4f
George Lewe <george@lewe.com>
2025-07-13 12:00:59 +0200

    Patterns controller/view optimization

--- src/views/patterns.php
+++ src/views/patterns.php
@@ -23,9 +23,7 @@ view.patterns
     ) {
       echo createAlertBox($alertData);
     }
-    $tabindex = 1;
-    $colsleft = 8;
-    $colsright = 4;
+    $tabindex = 0;
     ?>
 
     <div class="card">
@@ -38,12 +36,12 @@ view.patterns
       <div class="card-header text-white bg-<?= $CONF['controllers'][$controller]->panelColor ?>"><i class="<?= $CONF['controllers'][$controller]->faIcon ?> fa-lg me-3"></i><?= $LANG['ptn_list_title'] . $pageHelp ?></div>
       <div class="card-body">
 
-        <form class="form-control-horizontal" name="form_create" action="index.php?action=<?= $controller ?>" method="post" target="_self" accept-charset="utf-8">
-          <input name="csrf_token" type="hidden" value="<?= $_SESSION['csrf_token'] ?>">
+        <form class="form-control-horizontal" name="form_create" action="index.php?action=<?= htmlspecialchars($controller ?? '', ENT_QUOTES, 'UTF-8') ?>" method="post" target="_self" accept-charset="utf-8">
+          <input name="csrf_token" type="hidden" value="<?= htmlspecialchars($_SESSION['csrf_token'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
           <div class="row mb-4">
             <div class="col-lg-12 text-end">
               <br>
-              <a href="index.php?action=patternadd" class="btn btn-success" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_create_pattern'] ?></a>
+              <a href="index.php?action=patternadd" class="btn btn-success" tabindex="<?= ++$tabindex ?>" aria-label="<?= $LANG['btn_create_pattern'] ?>"><?= $LANG['btn_create_pattern'] ?></a>
             </div>
           </div>
 
@@ -62,20 +60,20 @@ view.patterns
           <?php
           foreach ($viewData['patterns'] as $pattern) : ?>
             <tr>
-              <td><?= $pattern['name'] ?></td>
-              <td><?= $pattern['description'] ?></td>
+              <td><?= htmlspecialchars($pattern['name'] ?? '', ENT_QUOTES, 'UTF-8') ?></td>
+              <td><?= htmlspecialchars($pattern['description'] ?? '', ENT_QUOTES, 'UTF-8') ?></td>
               <td><?= createPatternTable($pattern['id']) ?></td>
               <td class="align-top text-center">
-                <form class="form-control-horizontal" name="form_<?= $pattern['name'] ?>" action="index.php?action=patterns" method="post" target="_self" accept-charset="utf-8">
-                  <button type="button" class="btn btn-danger btn-sm" tabindex="<?= $tabindex++ ?>" data-bs-toggle="modal" data-bs-target="#modalDeletePattern_<?= $pattern['id'] ?>"><?= $LANG['btn_delete'] ?></button>
-                  <input name="hidden_id" type="hidden" value="<?= $pattern['id'] ?>">
-                  <input name="hidden_name" type="hidden" value="<?= $pattern['name'] ?>">
-                  <input name="hidden_description" type="hidden" value="<?= $pattern['description'] ?>">
-                  <a href="index.php?action=patternedit&amp;id=<?= $pattern['id'] ?>" class="btn btn-warning btn-sm" tabindex="<?= $tabindex++ ?>"><?= $LANG['btn_edit'] ?></a>
+                <form class="form-control-horizontal" name="form_<?= htmlspecialchars($pattern['name'] ?? '', ENT_QUOTES, 'UTF-8') ?>" action="index.php?action=patterns" method="post" target="_self" accept-charset="utf-8">
+                  <button type="button" class="btn btn-danger btn-sm" tabindex="<?= ++$tabindex ?>" data-bs-toggle="modal" data-bs-target="#modalDeletePattern_<?= htmlspecialchars($pattern['id'] ?? '', ENT_QUOTES, 'UTF-8') ?>"><?= $LANG['btn_delete'] ?></button>
+                  <input name="hidden_id" type="hidden" value="<?= htmlspecialchars($pattern['id'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
+                  <input name="hidden_name" type="hidden" value="<?= htmlspecialchars($pattern['name'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
+                  <input name="hidden_description" type="hidden" value="<?= htmlspecialchars($pattern['description'] ?? '', ENT_QUOTES, 'UTF-8') ?>">
+                  <a href="index.php?action=patternedit&amp;id=<?= htmlspecialchars($pattern['id'] ?? '', ENT_QUOTES, 'UTF-8') ?>" class="btn btn-warning btn-sm" tabindex="<?= ++$tabindex ?>"><?= $LANG['btn_edit'] ?></a>
 
                   <!-- Modal: Delete Pattern -->
-                  <?= createModalTop('modalDeletePattern_' . $pattern['id'], $LANG['modal_confirm']) ?>
-                  <?= $LANG['ptn_confirm_delete'] . $pattern['name'] ?> ?
+                  <?= createModalTop('modalDeletePattern_' . htmlspecialchars($pattern['id'] ?? '', ENT_QUOTES, 'UTF-8'), $LANG['modal_confirm']) ?>
+                  <?= htmlspecialchars($LANG['ptn_confirm_delete'], ENT_QUOTES, 'UTF-8') . htmlspecialchars($pattern['name'] ?? '', ENT_QUOTES, 'UTF-8') ?> ?
                   <?= createModalBottom('btn_patternDelete', 'danger', $LANG['btn_delete']) ?>
 
                 </form>
