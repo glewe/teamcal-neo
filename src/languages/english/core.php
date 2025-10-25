6c043879640bb8bae4990b3291e09a3dea9da3d1
George Lewe <george@lewe.com>
2025-10-25 19:27:06 +0200

    Language files update

--- src/languages/english/core.php
+++ src/languages/english/core.php
@@ -262,6 +262,28 @@ $LANG['btn_view'] = 'View';
 $LANG['configapp_title'] = 'Application Configuration';
 
 //
+// Email
+//
+$LANG['email_subject_absence_approval'] = 'TeamCal Neo Absence Approval Needed';
+$LANG['email_subject_group_changed'] = '%app_name% Group Changed';
+$LANG['email_subject_group_created'] = '%app_name% Group Created';
+$LANG['email_subject_group_deleted'] = '%app_name% Group Deleted';
+$LANG['email_subject_month_changed'] = '%app_name% Month Changed';
+$LANG['email_subject_month_created'] = '%app_name% Month Created';
+$LANG['email_subject_month_deleted'] = '%app_name% Month Deleted';
+$LANG['email_subject_password_reset'] = '%app_name% Password Reset';
+$LANG['email_subject_role_changed'] = '%app_name% Role Changed';
+$LANG['email_subject_role_created'] = '%app_name% Role Created';
+$LANG['email_subject_role_deleted'] = '%app_name% Role Deleted';
+$LANG['email_subject_user_account_changed'] = '%app_name% User Account Changed';
+$LANG['email_subject_user_account_created'] = '%app_name% User Account Created';
+$LANG['email_subject_user_account_deleted'] = '%app_name% User Account Deleted';
+$LANG['email_subject_user_account_mismatch'] = '%app_name% User Account Verification Mismatch';
+$LANG['email_subject_user_account_needs_approval'] = '%app_name% User Account Needs Approval';
+$LANG['email_subject_user_account_registered'] = '%app_name% User Account Registered';
+$LANG['email_subject_usercal_changed'] = 'TeamCal Neo User Calendar Changed';
+
+//
 // Error Messages
 //
 $LANG['error_title'] = 'Application Error';
