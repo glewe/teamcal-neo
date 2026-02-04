6173c2a6da63a43b7db9dcbdb0f9f650645c2d5b
George Lewe <george@lewe.com>
2026-02-04 21:26:50 +0100

    Update SQL update script

--- sql/update_4_to_5.0.0.sql
+++ sql/update_4_to_5.0.0.sql
@@ -1,17 +1,25 @@
 --
 -- Record deletions
 --
+DELETE FROM `tcneo_config` WHERE `name` = "cookieConsentCDN";
+DELETE FROM `tcneo_config` WHERE `name` = "faCDN";
 DELETE FROM `tcneo_config` WHERE `name` = "footerViewport";
+DELETE FROM `tcneo_config` WHERE `name` = "jQueryCDN";
 DELETE FROM `tcneo_config` WHERE `name` = "useCaptcha";
 
 --
 -- Record inserts
 --
+INSERT INTO `tcneo_config` (`name`, `value`) VALUES ('statsDefaultColorPresenceType', 'magenta');
 INSERT INTO `tcneo_config` (`name`, `value`) VALUES ('statsDefaultColorRemainder', 'orange');
+INSERT INTO `tcneo_config` (`name`, `value`) VALUES ('statsDefaultColorTrends', 'red');
+INSERT INTO `tcneo_config` (`name`, `value`) VALUES ('statsDefaultColorDayofweek', 'purple');
+INSERT INTO `tcneo_config` (`name`, `value`) VALUES ('statsDefaultColorDuration', 'orange');
 
 --
 -- Record updates
 --
+UPDATE `tcneo_config` SET `value` = 'sidebar' WHERE `name` = 'defaultMenu';
 UPDATE `tcneo_config` SET `value` = 'red' WHERE `name` = 'statsDefaultColorAbsences';
 UPDATE `tcneo_config` SET `value` = 'cyan' WHERE `name` = 'statsDefaultColorAbsencetype';
 UPDATE `tcneo_config` SET `value` = 'green' WHERE `name` = 'statsDefaultColorPresences';
